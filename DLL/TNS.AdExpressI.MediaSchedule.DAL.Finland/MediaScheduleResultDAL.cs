3b09ff6663a1a61a829b9492f83623b1b59ff8d6
Facon, Guillaume (KMCHM) <Guillaume.Facon@KantarMedia.com>
2008-08-22 11:59:12 +0000

    Adaptation Plan Media FI
    git-tfs-id: [http://172.17.236.41:8080/]$/AdExpress International Demo/Trunk;C4574

---
+++ DLL/TNS.AdExpressI.MediaSchedule.DAL.Finland/MediaScheduleResultDAL.cs
@@ -0,0 +1,96 @@
+#region Information
+/*
+ * Author : G. Facon & G. Ragneau
+ * Created : 24/08/2008
+ * Modifications :
+ *      Author - Date - Descriptopn
+ *      
+*/
+#endregion
+
+#region using
+using System;
+
+using TNS.AdExpress.Web.Core.Sessions;
+using TNS.AdExpress.Web.Core.Selection;
+
+using TNS.AdExpressI.MediaSchedule.DAL;
+using TNS.AdExpressI.MediaSchedule.DAL.Exceptions;
+using TNS.AdExpress.Constantes.Web;
+using TNS.AdExpress.Domain.Classification;
+using CstPeriod=TNS.AdExpress.Constantes.Web.CustomerSessions.Period;
+using CstDBClassif=TNS.AdExpress.Constantes.Classification.DB;
+#endregion
+
+namespace TNS.AdExpressI.MediaSchedule.DAL.Finland
+{
+    /// <summary>
+    /// Finish Implementation of IMediaScheduleResultDAL
+    /// </summary>
+    public class MediaScheduleResultDAL : DAL.MediaScheduleResultDAL
+    {
+
+        #region Constructor
+        /// <summary>
+        /// Default Constructor
+        /// </summary>
+        /// <param name="session">User session</param>
+        /// <param name="period">Report period</param>
+        public MediaScheduleResultDAL(WebSession session, MediaSchedulePeriod period):base(session, period){}
+        /// <summary>
+        /// Default Constructor
+        /// </summary>
+        /// <param name="session">User session</param>
+        /// <param name="period">Report period</param>
+        /// <param name="idVehicle">Id of the vehicle</param>
+        public MediaScheduleResultDAL(WebSession session, MediaSchedulePeriod period, Int64 idVehicle):base(session, period,idVehicle ){}
+        #endregion
+
+        #region Override
+        /// <summary>
+        /// Get field for periodicity data
+        /// </summary>
+        ///<param name="detailPeriod">Detail period type</param>
+        ///<param name="vehicleId">Vehicle Id</param>
+        /// <param name="displayPeriod">Result display level</param>
+        /// <returns>Field matching period type</returns>
+        protected override string GetPeriodicity(CustomerSessions.Period.PeriodBreakdownType detailPeriod,long vehicleId,CustomerSessions.Period.DisplayLevel displayPeriod) {
+
+
+            switch(detailPeriod) {
+                case CstPeriod.PeriodBreakdownType.month:
+                    return (" max(duration) as period_count ");
+                case CstPeriod.PeriodBreakdownType.week:
+                    return (" max(duration) as period_count ");
+                case CstPeriod.PeriodBreakdownType.data:
+                case CstPeriod.PeriodBreakdownType.data_4m:
+                    switch(VehiclesInformation.DatabaseIdToEnum(vehicleId)) {
+                        case CstDBClassif.Vehicles.names.press:
+                        case CstDBClassif.Vehicles.names.internationalPress:
+                            switch(displayPeriod) {
+                                case CstPeriod.DisplayLevel.monthly:
+                                    return string.Format(" max({0}DURATION_MONTH(date_media_num,duration)) as period_count ",_schAdexpr03.Sql);
+                                case CstPeriod.DisplayLevel.weekly:
+                                    return string.Format(" max({0}DURATION_WEEK(date_media_num,duration)) as period_count ",_schAdexpr03.Sql);
+                                default:
+                                    return (" (max(duration)/86400) as period_count ");
+                            }
+                        case CstDBClassif.Vehicles.names.outdoor:
+                        case CstDBClassif.Vehicles.names.radio:
+                        case CstDBClassif.Vehicles.names.tv:
+                        case CstDBClassif.Vehicles.names.others:
+                        case CstDBClassif.Vehicles.names.internet:
+                        case CstDBClassif.Vehicles.names.adnettrack:
+                            return (" 1 as period_count ");
+                        case CstDBClassif.Vehicles.names.directMarketing:
+                            return (" 7 as period_count ");
+                        default:
+                            throw (new MediaScheduleDALException("Unable to determine the media periodicity"));
+                    }
+                default:
+                    throw (new MediaScheduleDALException("Selected period detail unvalid. Unable to determine periodicity field."));
+            }
+        }
+        #endregion
+    }
+}
---
+++ DLL/TNS.AdExpressI.MediaSchedule.DAL.Finland/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Les informations générales relatives à un assembly dépendent de 
+// l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
+// associées à un assembly.
+[assembly: AssemblyTitle("TNS.AdExpressI.MediaSchedule.DAL.Finland")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("TNS")]
+[assembly: AssemblyProduct("TNS.AdExpressI.MediaSchedule.DAL.Finland")]
+[assembly: AssemblyCopyright("Copyright © TNS 2008")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// L'affectation de la valeur false à ComVisible rend les types invisibles dans cet assembly 
+// aux composants COM. Si vous devez accéder à un type dans cet assembly à partir de 
+// COM, affectez la valeur true à l'attribut ComVisible sur ce type.
+[assembly: ComVisible(false)]
+
+// Le GUID suivant est pour l'ID de la typelib si ce projet est exposé à COM
+[assembly: Guid("a15f2c78-1508-4932-b1e1-9c83ab7cb4e2")]
+
+// Les informations de version pour un assembly se composent des quatre valeurs suivantes :
+//
+//      Version principale
+//      Version secondaire 
+//      Numéro de build
+//      Révision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
