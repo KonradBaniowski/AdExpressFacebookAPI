f10596729470ecc08bbdc5e36e25cf09d517c385
Rkaina, Youssef (KMCHM) <Youssef.Rkaina@KantarMedia.com>
2008-09-24 11:59:56 +0000

    Utilisation des thèmes et du charset
    git-tfs-id: [http://172.17.236.41:8080/]$/AdExpress International Demo/Trunk;C5142

--- DLL/TNS.AdExpress.Anubis/Common/Functions.cs
+++ DLL/TNS.AdExpress.Anubis/Common/Functions.cs
@@ -13,6 +13,9 @@ using System.IO;
 using System.Text;
 
 using TNS.AdExpress.Anubis.Exceptions;
+using TNSAnubisConstantes = TNS.AdExpress.Anubis.Constantes;
+using TNS.AdExpress.Web.Core.Sessions;
+using TNS.AdExpress.Domain.Web;
 
 namespace TNS.AdExpress.Anubis.Common
 {
@@ -79,20 +82,26 @@ namespace TNS.AdExpress.Anubis.Common
 		/// Create a html file with a complete header, ready to receive html code after body
 		/// </summary>
 		/// <param name="path">File path</param>
+        /// <param name="webSession">Web Session</param>
 		/// <returns>link so as to write text in the file</returns>
-		public static StreamWriter GetHtmlFile(string path){
+        public static StreamWriter GetHtmlFile(string path, WebSession webSession) {
 			try{
+                string charSet = WebApplicationParameters.AllowedLanguages[webSession.SiteLanguage].Charset;
+                string themeName = WebApplicationParameters.Themes[webSession.SiteLanguage].Name;
+
 				if (!File.Exists(path)){
 					StreamWriter sw = File.CreateText(path);
 					sw.WriteLine("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0 Transitional//EN\" >");
 					sw.WriteLine("<HTML>");
 					sw.WriteLine("<HEAD>");
-					sw.WriteLine("<META http-equiv=\"Content-Type\" content=\"text/html; charset=windows-1252\">");
+                    sw.WriteLine("<META http-equiv=\"Content-Type\" content=\"text/html; charset=" + charSet + "\">");
 					sw.WriteLine("<meta content=\"Microsoft Visual Studio .NET 7.1\" name=\"GENERATOR\">");
 					sw.WriteLine("<meta content=\"C#\" name=\"CODE_LANGUAGE\">");
 					sw.WriteLine("<meta content=\"JavaScript\" name=\"vs_defaultClientScript\">");
 					sw.WriteLine("<meta content=\"http://schemas.microsoft.com/intellisense/ie5\" name=\"vs_targetSchema\">");
-					sw.WriteLine("<LINK href=\"http://www.tnsadexpress.com/Css/AdExpress.css\" type=\"text/css\" rel=\"stylesheet\">");
+                    sw.WriteLine("<LINK href=\"" + TNSAnubisConstantes.Result.CSS_LINK + "/" + themeName + "/Css/AdExpressFr.css\" type=\"text/css\" rel=\"stylesheet\">");
+                    sw.WriteLine("<LINK href=\"" + TNSAnubisConstantes.Result.CSS_LINK + "/" + themeName + "/Css/GenericUI.css\" type=\"text/css\" rel=\"stylesheet\">");
+                    sw.WriteLine("<LINK href=\"" + TNSAnubisConstantes.Result.CSS_LINK + "/" + themeName + "/Css/MediaSchedule.css\" type=\"text/css\" rel=\"stylesheet\">");
 					sw.WriteLine("<meta http-equiv=\"expires\" content=\"Wed, 23 Feb 1999 10:49:02 GMT\">");
 					sw.WriteLine("<meta http-equiv=\"expires\" content=\"0\">");
 					sw.WriteLine("<meta http-equiv=\"pragma\" content=\"no-cache\">");
