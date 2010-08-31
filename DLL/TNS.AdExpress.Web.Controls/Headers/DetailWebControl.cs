30c14909b6e2788e7e32cf0831d44b9ba68db3db
Masson, Benjamin (KMCHM) <Benjamin.Masson@KantarMedia.com>
2010-08-31 10:42:58 +0000

    Archive (ResultOptionWebControl)
    git-tfs-id: [http://172.17.236.41:8080/]$/AdExpress International Demo/Trunk;C15956

--- DLL/TNS.AdExpress.Web.Controls/Headers/DetailWebControl.cs
+++ DLL/TNS.AdExpress.Web.Controls/Headers/DetailWebControl.cs
@@ -4,20 +4,19 @@
 #endregion
 
 using System;
-using System.Web.UI;
-using System.Web.UI.WebControls;
+using System.Collections;
 using System.ComponentModel;
 using System.Drawing;
-using System.Collections;
+using System.Web.UI;
+using System.Web.UI.WebControls;
 using TNS.AdExpress.Domain.Translation;
-using TNS.AdExpress.Web.Core.Sessions;
 using TNS.AdExpress.Domain.Web.Navigation;
 using TNS.AdExpress.Web.Controls.Results;
-
-using SessionCst = TNS.AdExpress.Constantes.Web.CustomerSessions;
-using CustomerCst = TNS.AdExpress.Constantes.Customer.Right;
-using WebFunctions = TNS.AdExpress.Web.Functions;
+using TNS.AdExpress.Web.Core.Sessions;
 using ClassificationCst = TNS.AdExpress.Constantes.Classification;
+using CustomerCst = TNS.AdExpress.Constantes.Customer.Right;
+using SessionCst = TNS.AdExpress.Constantes.Web.CustomerSessions;
+using WebFunctions = TNS.AdExpress.Web.Functions;
 
 namespace TNS.AdExpress.Web.Controls.Headers{
 	/// <summary>
@@ -111,41 +110,70 @@ namespace TNS.AdExpress.Web.Controls.Headers{
 		/// Initialisation
 		/// </summary>
 		/// <param name="e">Arguments</param>
-		protected override void OnInit(EventArgs e){
+		protected override void OnInit(EventArgs e)
+        {
+            productDetail = new RadioButtonList();
+            productDetail.Width = new System.Web.UI.WebControls.Unit("100%");
+            productDetail.Items.Add(new ListItem(GestionWeb.GetWebWord(1106, customerWebSession.SiteLanguage), "AdvertiserBrandProduct_" + SessionCst.PreformatedDetails.PreformatedProductDetails.advertiser.GetHashCode().ToString()));
+            productDetail.Items.Add(new ListItem(GestionWeb.GetWebWord(1889, customerWebSession.SiteLanguage), "AdvertiserBrandProduct_" + SessionCst.PreformatedDetails.PreformatedProductDetails.brand.GetHashCode().ToString()));
+            if(_showProduct)
+                productDetail.Items.Add(new ListItem(GestionWeb.GetWebWord(858, customerWebSession.SiteLanguage), "AdvertiserBrandProduct_" + SessionCst.PreformatedDetails.PreformatedProductDetails.product.GetHashCode().ToString()));
+            productDetail.ID = "productDetail_" + this.ID;
+            productDetail.AutoPostBack = AutoPostBackOption;
+            productDetail.CssClass = CommonCssClass;
+            Controls.Add(productDetail);
+
+            if((customerWebSession.PreformatedProductDetail != SessionCst.PreformatedDetails.PreformatedProductDetails.advertiser)
+                && (customerWebSession.PreformatedProductDetail != SessionCst.PreformatedDetails.PreformatedProductDetails.brand)
+                && (customerWebSession.PreformatedProductDetail != SessionCst.PreformatedDetails.PreformatedProductDetails.product)) {
+
+                customerWebSession.PreformatedProductDetail = TNS.AdExpress.Constantes.Web.CustomerSessions.PreformatedDetails.PreformatedProductDetails.advertiser;
+            }
 		}
 		#endregion
 
-		#region Custom PreRender
+        #region OnLoad
+        /// <summary>
+        /// OnLoad
+        /// </summary>
+        /// <param name="e">Args</param>
+        protected override void OnLoad(EventArgs e) {
+            base.OnLoad(e);
 
-		/// <summary>
+            if(productDetail.Items.FindByValue("AdvertiserBrandProduct_" + SessionCst.PreformatedDetails.PreformatedProductDetails.advertiser.GetHashCode().ToString()) != null
+                && productDetail.Items.FindByValue("AdvertiserBrandProduct_" + SessionCst.PreformatedDetails.PreformatedProductDetails.advertiser.GetHashCode().ToString()).Selected
+                ) {
+                customerWebSession.PreformatedProductDetail = SessionCst.PreformatedDetails.PreformatedProductDetails.advertiser;
+            }
+            else if(productDetail.Items.FindByValue("AdvertiserBrandProduct_" + SessionCst.PreformatedDetails.PreformatedProductDetails.brand.GetHashCode().ToString()) != null
+                && productDetail.Items.FindByValue("AdvertiserBrandProduct_" + SessionCst.PreformatedDetails.PreformatedProductDetails.brand.GetHashCode().ToString()).Selected
+                ) {
+                customerWebSession.PreformatedProductDetail = SessionCst.PreformatedDetails.PreformatedProductDetails.brand;
+            }
+            else if(productDetail.Items.FindByValue("AdvertiserBrandProduct_" + SessionCst.PreformatedDetails.PreformatedProductDetails.product.GetHashCode().ToString()) != null
+                && productDetail.Items.FindByValue("AdvertiserBrandProduct_" + SessionCst.PreformatedDetails.PreformatedProductDetails.product.GetHashCode().ToString()).Selected
+                ) {
+                customerWebSession.PreformatedProductDetail = SessionCst.PreformatedDetails.PreformatedProductDetails.product;
+            }
+        }
+        #endregion
+
+        #region Custom PreRender
+        /// <summary>
 		///custom prerender 
 		/// </summary>
 		/// <param name="sender">object qui lance l'évènement</param>
 		/// <param name="e">arguments</param>
-		private void Custom_PreRender(object sender, System.EventArgs e){
-
-			#region régie ou centred'intêret ou Titre
-			//Option détail Produit
-			if(advertiserBrandProductOption){
-				productDetail = new RadioButtonList();
-				productDetail.Width = new System.Web.UI.WebControls.Unit("100%");
-				productDetail.Items.Add(new ListItem(GestionWeb.GetWebWord(1106, customerWebSession.SiteLanguage),"AdvertiserBrandProduct_"+SessionCst.PreformatedDetails.PreformatedProductDetails.advertiser.GetHashCode().ToString()));
-				productDetail.Items.Add(new ListItem(GestionWeb.GetWebWord(1889, customerWebSession.SiteLanguage),"AdvertiserBrandProduct_"+SessionCst.PreformatedDetails.PreformatedProductDetails.brand.GetHashCode().ToString()));
-				if(_showProduct)productDetail.Items.Add(new ListItem(GestionWeb.GetWebWord(858, customerWebSession.SiteLanguage),"AdvertiserBrandProduct_"+SessionCst.PreformatedDetails.PreformatedProductDetails.product.GetHashCode().ToString()));
-				productDetail.ID = "productDetail_"+this.ID;
-				productDetail.AutoPostBack=AutoPostBackOption;
-				productDetail.CssClass=CommonCssClass;
-				try{
-					productDetail.Items.FindByValue("AdvertiserBrandProduct_"+customerWebSession.PreformatedProductDetail.GetHashCode().ToString()).Selected = true;
-				}
-				catch(System.Exception){
-					productDetail.Items[0].Selected = true;
-				}
-				
-				Controls.Add(productDetail);
-			}
-			#endregion
-		}
+        private void Custom_PreRender(object sender, System.EventArgs e) {
+            if(advertiserBrandProductOption) {
+                try {
+                    productDetail.Items.FindByValue("AdvertiserBrandProduct_" + customerWebSession.PreformatedProductDetail.GetHashCode().ToString()).Selected = true;
+                }
+                catch(System.Exception) {
+                    productDetail.Items[0].Selected = true;
+                }
+            }
+        }
 		#endregion
 
 		#region Render
@@ -155,7 +183,8 @@ namespace TNS.AdExpress.Web.Controls.Headers{
 		/// <param name="output"> Le writer HTML vers lequel écrire </param>
 		protected override void Render(HtmlTextWriter output){
 			output.Write("\n<table cellSpacing=\"0\" cellPadding=\"0\" width=\"100%\" border=\"0\">");
-			//Descriptif
+			
+            //Descriptif
 			output.Write("\n<TR>");
 			output.Write("\n<TD height=\"5\"></TD>");
 			output.Write("\n</TR>");
@@ -168,7 +197,7 @@ namespace TNS.AdExpress.Web.Controls.Headers{
 			output.Write("\n<TD height=\"5\"></TD>");
 			output.Write("\n</TR>");
 
-			//détail produit
+			// Détail produit
 			if (advertiserBrandProductOption){
 				output.Write("\n<tr>");
                 output.Write("\n<td class=\"txtBlanc11Bold\">");
