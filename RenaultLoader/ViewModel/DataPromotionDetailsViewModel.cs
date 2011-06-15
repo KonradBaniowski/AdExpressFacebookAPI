f7a9afb133f4e4e24ca6ae7b22a24a8e7749e321
Mussuma, Dédé (KMCHM) <Dede.Mussuma@KantarMedia.com>
2011-06-15 11:52:18 +0000

    Ajout projet Renault Loader
    git-tfs-id: [http://172.17.236.41:8080/]$/AdExpress International Demo/Trunk;C19901

---
+++ RenaultLoader/ViewModel/DataPromotionDetailsViewModel.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+using TNS.AdExpress.Domain.Results;
+using System.IO;
+using Microsoft.Win32;
+
+namespace RenaultLoader.ViewModel
+{
+    public class DataPromotionDetailsViewModel : INotifyPropertyChanged
+    {
+     
+
+        private FileInfo _fInfo;
+
+        private RelayCommand _openCommand;
+        private RelayCommand _saveCommand;
+
+
+        #region Commands
+
+        #endregion
+
+        #region  Properties
+        public RelayCommand OpenCommand
+        {
+            get
+            {
+                if (this._openCommand == null)
+                    this._openCommand = new RelayCommand(ExecuteOpenFileDialog);
+
+                return this._openCommand;
+            }
+        }
+        public RelayCommand SaveCommand
+        {
+            get
+            {
+                if (this._saveCommand == null)
+                    this._saveCommand = new RelayCommand(ExecuteSave);
+
+                return this._saveCommand;
+            }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        public FileInfo CurrentFile
+        {
+            get { return _fInfo; }
+            set
+            {
+                _fInfo = value;
+                RaisePropertyChanged("CurrentFile");
+            }
+        }
+
+        #endregion
+
+        #region INotifyPropertyChanged Members
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void RaisePropertyChanged(string pstrPropertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new System.ComponentModel.PropertyChangedEventArgs(pstrPropertyName));
+
+        }
+        #endregion
+
+       #region Default Constructor
+        /// <summary>
+        /// 
+        /// </summary>
+        public DataPromotionDetailsViewModel()
+        {
+        }
+        #endregion
+
+
+        private void ExecuteOpenFileDialog()
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog { };
+            openFileDialog.Multiselect = false;
+            openFileDialog.Filter = "Excel Files (.xls;*xlsx)|*.xls;*xlsx";
+            openFileDialog.FilterIndex = 1;
+            openFileDialog.CheckFileExists = true;
+
+            if (openFileDialog.ShowDialog().Value)
+                CurrentFile = new FileInfo(openFileDialog.FileName);
+
+        }
+        private void ExecuteSave()
+        {
+            //
+        }
+       
+    }
+}
---
+++ RenaultLoader/Window1.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+
+namespace RenaultLoader
+{
+    /// <summary>
+    /// Logique d'interaction pour Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            this.DataContext = new ViewModel.DataPromotionDetailsViewModel();
+            InitializeComponent();
+            
+        }
+
+        //private void btnBrowse_Click(object sender, RoutedEventArgs e)
+        //{
+        //    OpenFileDialog fdlg = new OpenFileDialog();
+        //    fdlg.Title = "File Upload";
+        //    fdlg.Filter = "Xml File|*.xml";
+        //    //fdlg.FilterIndex = 2;
+        //    fdlg.RestoreDirectory = true;
+        //    Nullable<bool> result = fdlg.ShowDialog();
+        //    if (result == true)
+        //    {
+        //        txtFileUpload.Text = fdlg.FileName;
+        //    }
+        //}
+    }
+}
