b459032a8b79fd9ea176b5ca1e0ec79c1581b502
Rousseau, Alexandre (KMCHM) <Alexandre.Rousseau@KantarMedia.com>
2012-04-03 16:16:23 +0000

    Mise en Place de CZ
    git-tfs-id: [http://172.17.236.41:8080/]$/AdExpress International Demo/Trunk;C27903

---
+++ AresService/TNS.Ares.AdExpress.PdfSectorielleSrvCz/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+
+namespace TNS.Ares.AdExpress.PdfSectorielleSrvCz
+{
+    static class Program
+    {
+        /// <summary>
+        /// Point d'entrée principal de l'application.
+        /// </summary>
+        static void Main()
+        {
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[] 
+			{ 
+				new PdfSectorielleService() 
+			};
+            ServiceBase.Run(ServicesToRun);
+        }
+    }
+}
---
+++ AresService/TNS.Ares.AdExpress.PdfSectorielleSrvCz/ProjectInstaller.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace TNS.Ares.AdExpress.PdfSectorielleSrvCz
+{
+    partial class ProjectInstaller
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur de composants
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.serviceProcessInstaller = new System.ServiceProcess.ServiceProcessInstaller();
+            this.aresPdfSectorielleServiceInstallerCz = new System.ServiceProcess.ServiceInstaller();
+            // 
+            // serviceProcessInstaller
+            // 
+            this.serviceProcessInstaller.Account = System.ServiceProcess.ServiceAccount.LocalService;
+            this.serviceProcessInstaller.Password = null;
+            this.serviceProcessInstaller.Username = null;
+            // 
+            // aresPdfSectorielleServiceInstallerCz
+            // 
+            this.aresPdfSectorielleServiceInstallerCz.Description = "Generates AdExpress Module Analyse Sectorielle Pdf Cz";
+            this.aresPdfSectorielleServiceInstallerCz.DisplayName = "Ares PdfSectorielle Service (Hotep) Cz";
+            this.aresPdfSectorielleServiceInstallerCz.ServiceName = "Ares PdfSectorielle Service (Hotep) Cz";
+            // 
+            // ProjectInstaller
+            // 
+            this.Installers.AddRange(new System.Configuration.Install.Installer[] {
+            this.serviceProcessInstaller,
+            this.aresPdfSectorielleServiceInstallerCz});
+
+        }
+
+        #endregion
+
+        private System.ServiceProcess.ServiceProcessInstaller serviceProcessInstaller;
+        private System.ServiceProcess.ServiceInstaller aresPdfSectorielleServiceInstallerCz;
+    }
+}
