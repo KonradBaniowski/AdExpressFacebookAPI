471f61efeae24fffdda1d52ae0afe76051407821
Mussuma, Dédé (KMCHM) <Dede.Mussuma@KantarMedia.com>
2012-04-26 16:23:03 +0000

    git-tfs-id: [http://172.17.236.41:8080/]$/AdExpress International Demo/Trunk;C28883

---
+++ AresService/TNS.Ares.AdExpress.PdfCelebritiesSrv/ProjectInstaller.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace TNS.Ares.AdExpress.PdfCelebritiesSrv
+{
+    partial class ProjectInstaller
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur de composants
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.serviceProcessInstaller = new System.ServiceProcess.ServiceProcessInstaller();
+            this.aresPdfCelebritiesServiceInstaller = new System.ServiceProcess.ServiceInstaller();
+            // 
+            // serviceProcessInstaller
+            // 
+            this.serviceProcessInstaller.Account = System.ServiceProcess.ServiceAccount.LocalSystem;
+            this.serviceProcessInstaller.Password = null;
+            this.serviceProcessInstaller.Username = null;
+            // 
+            // aresPdfPMServiceInstaller
+            // 
+            this.aresPdfCelebritiesServiceInstaller.Description = "Generates Celebrities Pdf";
+            this.aresPdfCelebritiesServiceInstaller.DisplayName = "Ares Pdf Celebrities Service Apis";
+            this.aresPdfCelebritiesServiceInstaller.ServiceName = "Ares Pdf Celebrities Service Apis";
+            this.aresPdfCelebritiesServiceInstaller.StartType = System.ServiceProcess.ServiceStartMode.Automatic;
+            // 
+            // ProjectInstaller
+            // 
+            this.Installers.AddRange(new System.Configuration.Install.Installer[] {
+            this.serviceProcessInstaller,
+            this.aresPdfCelebritiesServiceInstaller});
+        }
+
+        #endregion
+
+        private System.ServiceProcess.ServiceProcessInstaller serviceProcessInstaller;
+        private System.ServiceProcess.ServiceInstaller aresPdfCelebritiesServiceInstaller;
+    }
+}
