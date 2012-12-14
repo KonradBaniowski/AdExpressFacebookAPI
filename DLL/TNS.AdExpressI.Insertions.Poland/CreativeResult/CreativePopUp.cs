b2b78d3f626fac112c2645ce31fad70874a17bd3
Mussuma, Dédé (KMCHM) <Dede.Mussuma@KantarMedia.com>
2012-12-14 13:45:04 +0000

    Création Plugin TNS.AdExpress.Insertions.Poland pour la gestion des insertions en pologne
    git-tfs-id: [http://172.17.236.41:8080/]$/AdExpress International Demo/Trunk;C34797

---
+++ DLL/TNS.AdExpressI.Insertions.Poland/CreativeResult/CreativePopUp.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Web.UI;
+using TNS.AdExpress.Constantes.Classification.DB;
+using TNS.AdExpress.Domain.Translation;
+using TNS.AdExpress.Web.Core.Sessions;
+using CstClassification = TNS.AdExpress.Constantes.Classification;
+using CstWeb = TNS.AdExpress.Constantes.Web;
+namespace TNS.AdExpressI.Insertions.Poland.CreativeResult
+{
+    public class CreativePopUp : Insertions.CreativeResult.CreativePopUp
+    {
+        #region Constructor
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="vehicle">Vehicle</param>
+        /// <param name="idSlogan">Slogan identifier</param>
+        /// <param name="file">File</param>
+        /// <param name="webSession">WebSession</param>
+        /// <param name="title">Title</param>
+        /// <param name="hasCreationReadRights">True if has Creation Read Rights</param>
+        /// <param name="hasCreationDownloadRights">true if has Creation Download Rights</param>
+        public CreativePopUp(Page popUp, Vehicles.names vehicle, string idSlogan, string file, WebSession webSession,
+                             string title, bool hasCreationReadRights, bool hasCreationDownloadRights) :
+                                 base(
+                                 popUp, vehicle, idSlogan, file, webSession, title, hasCreationReadRights,
+                                 hasCreationDownloadRights)
+        {
+        }
+
+        #endregion
+
+        readonly Func<string, string, string,bool> isCreativeExists = (s, e,v) =>
+                      File.Exists(string.Format("{0}\\{3}\\{1}\\{2}.{3}"
+                     , s, v.Substring(0, 3), v, e));
+
+        readonly Func<string, string, string,string> getCreativePath = (s, e,v) => 
+            string.Format("{0}/{3}/{1}/{2}.{3}",s, v.Substring(0, 3), v, e);
+
+        /// <summary>
+        /// Get Creative Pathes
+        /// </summary>
+        /// <param name="realFormatFound">True if real Format Found</param>
+        /// <param name="windowsFormatFound">True if windows Forma tFound</param>       
+        protected override void GetCreativePathes(ref bool realFormatFound, ref bool windowsFormatFound)
+        {
+          
+            switch (_vehicle)
+            {
+                case Vehicles.names.radio:
+
+                    //Vérification de l'existence du fichier real
+                    if (isCreativeExists(CstWeb.CreationServerPathes.LOCAL_PATH_CREATIVES_RADIO, RA_EXTENSION,_idSlogan))
+                        realFormatFound = true;
+
+                    //Vérification de l'existence du fichier wm
+                    if (isCreativeExists(CstWeb.CreationServerPathes.LOCAL_PATH_CREATIVES_RADIO, WMA_EXTENSION, _idSlogan))
+                        windowsFormatFound = true;
+
+
+                    //Construction des chemins real et wm					
+                    if (_hasCreationReadRights)
+                    {
+                        //Fichiers en lectures (streaming)
+                        _pathDownloadingRealFile =
+                            _pathReadingRealFile =
+                            getCreativePath(CstWeb.CreationServerPathes.DOWNLOAD_RADIO_SERVER, RA_EXTENSION, _idSlogan);
+                         
+                        _pathDownloadingWindowsFile =
+                            _pathReadingWindowsFile = getCreativePath(CstWeb.CreationServerPathes.DOWNLOAD_RADIO_SERVER,
+                            WMA_EXTENSION, _idSlogan);
+                         
+                    }
+                    break;
+
+                case Vehicles.names.tv:
+
+                    if (isCreativeExists(CstWeb.CreationServerPathes.LOCAL_PATH_VIDEO, RM_EXTENSION, _file))
+                        realFormatFound = true;
+
+                    if (isCreativeExists(CstWeb.CreationServerPathes.LOCAL_PATH_VIDEO, WMV_EXTENSION, _file))
+                        windowsFormatFound = true;
+
+                    //Construction des chemins real et wm	
+
+                    if (_hasCreationReadRights)
+                    {
+                        //Fichiers à télécharger 
+                        _pathReadingRealFile =  _pathDownloadingRealFile =
+                            getCreativePath(CstWeb.CreationServerPathes.DOWNLOAD_TV_SERVER, RM_EXTENSION, _file);
+
+                        _pathReadingWindowsFile = _pathDownloadingWindowsFile =
+                            getCreativePath(CstWeb.CreationServerPathes.DOWNLOAD_TV_SERVER, WMV_EXTENSION, _file);
+                         
+                    }
+                    break;
+
+
+                default:
+                    _webSession.Source.Close();
+                    _popUp.Response.Redirect(string.Format("/Public/Message.aspx?msgTxt={0}&title={1}",
+                                                           GestionWeb.GetWebWord(890, _webSession.SiteLanguage),
+                                                           GestionWeb.GetWebWord(887, _webSession.SiteLanguage)));
+                    break;
+            }
+        }
+    }
+}
---
+++ DLL/TNS.AdExpressI.Insertions.Poland/InsertionsResult.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Data;
+using TNS.AdExpress.Constantes.Web;
+using TNS.AdExpress.Web.Core.Sessions;
+
+namespace TNS.AdExpressI.Insertions.Poland
+{
+    public class InsertionsResult : Insertions.InsertionsResult
+    {
+        #region Constructor
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="session">User session</param>
+        /// <param name="moduleId">Module ID</param>
+        public InsertionsResult(WebSession session, long moduleId)
+            : base(session, moduleId)
+        {
+        }
+
+        #endregion
+
+        /// <summary>
+        /// Check if visual is available
+        /// </summary>
+        /// <param name="row">Data  row</param>
+        /// <param name="idMedia">Id vehicle</param>
+        /// <param name="dateCoverNum">cover date</param>
+        /// <param name="dateMediaNum">date</param>
+        /// <returns>True if viual is available</returns>
+        protected override bool IsVisualAvailable(DataRow row, out long idMedia, out long dateCoverNum, out long dateMediaNum)
+        {
+            Int64 disponibility = -1;
+            Int64 activation = -1;
+            dateCoverNum = -1;
+            dateMediaNum = -1;
+            idMedia = -1;
+
+            if (row.Table.Columns.Contains("id_media") && row["id_media"] != DBNull.Value)
+            {
+                idMedia = Convert.ToInt64(row["id_media"]);
+            }
+            if (row.Table.Columns.Contains("date_media_num") && row["date_media_num"] != DBNull.Value)
+            {
+                dateMediaNum = Convert.ToInt64(row["date_media_num"]);
+            }
+            if (row.Table.Columns.Contains("dateKiosque") && row["dateKiosque"] != DBNull.Value)
+            {
+                dateMediaNum = Convert.ToInt64(row["dateKiosque"]);
+            }
+            return (idMedia > 0 && row["visual"] != DBNull.Value);
+        }
+
+        #region GetCreativePathPress
+        /// <summary>
+        /// Get Creative Path Press
+        /// </summary>
+        /// <param name="file">file</param>
+        /// <param name="idMedia">id Media</param>
+        /// <param name="dateCoverNum">date Cover 
+        /// </param>
+        /// <param name="bigSize">true if big size else false</param>
+        /// <param name="dateMediaNum">date Media </param>
+        /// <returns>creative path</returns>
+        protected override string GetCreativePathPress(string file, Int64 idMedia, Int64 dateCoverNum, bool bigSize, Int64 dateMediaNum)
+        {         
+
+            return string.Format("{0}/{1}/{2}/{3}", CreationServerPathes.IMAGES, idMedia, dateMediaNum, file);
+
+        }
+        #endregion
+
+        protected override string GetPressVignettes(DataRow currentRow, string dateField, string vignettes,
+                                              string imagesList)
+        {
+            bool first = true;
+            var fileList = currentRow["visual"].ToString().Split(',');
+
+           
+            string pathWebImagette = string.Format("{0}/{1}/{2}/", CreationServerPathes.IMAGES, currentRow["id_media"].ToString()
+                , currentRow[dateField].ToString());
+            string pathWeb = string.Format("{0}/{1}/{2}/", CreationServerPathes.IMAGES, currentRow["id_media"].ToString()
+                , currentRow[dateField].ToString());
+
+            foreach (string file in fileList)
+            {
+                vignettes += string.Format("<img src='{0}{1}' border=\"0\" width=\"50\" height=\"64\" >", pathWebImagette, file);
+                if (first) imagesList = string.Format("{0}{1}", pathWeb, file);
+                else
+                {
+                    imagesList += string.Format(",{0}{1}", pathWeb, file);
+                }
+                first = false;
+            }
+
+            if (vignettes.Length > 0)
+            {
+                vignettes = string.Format("<a href=\"javascript:openPressCreation('{0}');\">{1}</a>", imagesList.Replace("/Imagette", ""), vignettes);
+                vignettes += "\n<br>";
+            }
+            return vignettes;
+        }
+
+
+
+    }
+}
