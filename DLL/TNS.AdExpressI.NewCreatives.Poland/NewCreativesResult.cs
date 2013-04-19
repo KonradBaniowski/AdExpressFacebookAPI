f8a0668e878f2482f45b4c34d4a096c8c17f9685
Mussuma, Dédé (KMCHM) <Dede.Mussuma@KantarMedia.com>
2013-04-19 12:10:18 +0000

    git-tfs-id: [http://172.17.236.41:8080/]$/AdExpress International Demo/Trunk;C36970

---
+++ DLL/TNS.AdExpressI.NewCreatives.Poland/NewCreativesResult.cs
@@ -0,0 +1,31 @@
+﻿using TNS.AdExpress.Web.Core.Result;
+using TNS.AdExpress.Web.Core.Sessions;
+using TNS.FrameWork.WebResultUI;
+
+namespace TNS.AdExpressI.NewCreatives.Poland
+{
+    public class NewCreativesResult:NewCreatives.NewCreativesResult {
+        #region Constructor
+        /// <summary>
+        /// Default Constructor
+        /// </summary>
+        /// <param name="session">User session</param>
+        public NewCreativesResult(WebSession session)
+            : base(session)
+        {
+        }
+        #endregion
+
+        protected override bool CanShowCreative()
+        {
+            return _vehicleInformation.ShowCreations;
+        }
+
+        protected override void SetCellCreativesLink(ResultTable tab, int iCurLine, int iCol, AdExpressCellLevel[] cellLevels, int i)
+        {
+            tab[iCurLine, iCol] = new TNS.AdExpress.Web.Core.Poland.Result.CellCreativesLink(cellLevels[i],
+                _webSession, _webSession.GenericProductDetailLevel, string.Empty, -1);
+        }
+
+    }
+}
