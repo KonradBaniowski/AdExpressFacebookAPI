2ec5c693dc548869517bcaf5a0d4f8fccea9ab74
Mussuma, Dédé (KMCHM) <Dede.Mussuma@KantarMedia.com>
2015-07-24 14:26:01 +0000

    git-tfs-id: [http://172.17.236.41:8080/]$/AdExpress International Demo/Trunk;C49641

---
+++ DLL/TNS.AdExpressI.Insertions/CreativeResult/ICreativePopUp.cs
@@ -0,0 +1,42 @@
+﻿namespace TNS.AdExpressI.Insertions.CreativeResult
+{
+    public interface ICreativePopUp
+    {
+        /// <summary>
+        /// Vérifie si fichier video trouvé
+        /// </summary>
+        bool IsVideoFileFound { get; set; }
+
+        /// <summary>
+        /// Vérifie si fichier audio trouvé
+        /// </summary>
+        bool IsAudioFileFound { get; set; }
+
+        /// <summary>
+        /// Identifiant Produit
+        /// </summary>
+        string IdProduct { get; set; }
+
+        /// <summary>
+        /// Indique si l'utilisateur à le droit de lire les créations
+        /// </summary>
+        bool HasCreationReadRights { get; set; }
+
+        /// <summary>
+        /// Indique si l'utilisateur à le droit de télécharger les créations
+        /// </summary>
+        bool HasCreationDownloadRights { get; set; }
+
+        /// <summary>
+        /// Get creative popup Html code
+        /// </summary>
+        /// <returns>Html code</returns>
+        string CreativePopUpRender();
+
+        /// <summary>
+        /// Get creative popup Html code
+        /// </summary>
+        /// <returns>Html code</returns>
+        string CreativePopUpRenderWithoutOptions(int width, int height);
+    }
+}
