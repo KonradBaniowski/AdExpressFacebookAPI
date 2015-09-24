979c545dcd4ea64abec402cf6d4e91c6ef37da2d
dedson <dede.mussuma@kantarmedia.com>
2015-09-24 16:34:27 +0200

    AdExpress Audio Video Converter Engine

---
+++ AresService/KM.AdExpress.AudioVideoConverter/Engine.VideoConvert.cs
@@ -0,0 +1,234 @@
+﻿using KM.AdExpress.AudioVideoConverter.DAL;
+using KM.AdExpress.AudioVideoConverter.Tools;
+using LinkSystem.LinkKernel.Core;
+using LinkSystem.LinkKernel.CoreMonitor;
+using LinkSystem.LinkKernel.Enums;
+using LinkSystem.LinkMonitor;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Xml.Linq;
+
+namespace KM.AdExpress.AudioVideoConverter
+{
+   public partial class Engine
+    {
+       private void DoVideoConversion(TaskExecution taskExecution, CancellationToken cancellationToken,
+                                       ManualResetEventSlim pausEvent, string userToken)
+       {
+           var taskLogHeader = string.Format("The DoVideoConversion task '{0}' t=[{1}]", taskExecution.Name, userToken);
+           string arguments = "  -acodec libfaac -aq 200 -vcodec libx264 -s {0} -aspect 16:9 ";
+
+
+           AdvancedTrace.TraceInformation(string.Format("{0} is running ...", taskLogHeader), ModuleName);
+           try
+           {
+               if (UseImpersonate)
+               {
+                   OpenImpersonation();
+               }
+
+               const long ID_VEHICLE_TV = 3;
+               var xTask = XElement.Parse(taskExecution.XMLTask);
+               var dal = new CreativeDAL(_connectionString, _providerDataAccess);
+               using (var db = dal.AudioVideoConverterDB.CreateDbManager())
+               {
+                   DateTime dateCreationBeginning = DateTime.Now.AddDays(-1);
+                   if (xTask.Attributes().Any(p => p.Name == "DATE_CREATION_BEGIN"))
+                   {
+                       dateCreationBeginning = DateTimeHelpers.YYYYMMDDToDateTime((string)xTask.Attribute("DATE_CREATION_BEGIN"));
+                   }
+
+                   DateTime dateCreationEnd = DateTime.Now;
+                   if (xTask.Attributes().Any(p => p.Name == "DATE_CREATION_END"))
+                   {
+                       dateCreationEnd = DateTimeHelpers.YYYYMMDDToDateTime((string)xTask.Attribute("DATE_CREATION_END"));
+                   }
+
+
+
+                   var creatives = dal.GetCreative(db, dateCreationBeginning, dateCreationEnd, ID_VEHICLE_TV);
+                  
+
+                   string temPath = Path.Combine(Path.GetTempPath(), string.Format(@"AdExTvCreative_{0}\", DateTime.Now.ToString("yyyyMMdd")));
+
+                   if (creatives.Any() && !Directory.Exists(temPath))
+                   {
+                       Directory.CreateDirectory(temPath);
+                       AdvancedTrace.TraceInformation(string.Format("{0} television files to convert ...", creatives.Count()), ModuleName);
+                   }
+                   ReportGenerator reportGenerator = new ReportGenerator(dateCreationBeginning, dateCreationEnd,  "TELEVISION");
+                   reportGenerator.NbAllFiles = creatives.Count();
+
+                   List<string> notExistFilesSource = new List<string>();
+                   List<string> filesNotConverted = new List<string>();
+                   List<string> filesExisting = new List<string>();
+
+                   creatives.ForEach(p =>
+                   {
+                       if (!string.IsNullOrEmpty(p.FileName) && p.IdMultimedia > 0)
+                       {
+                           AdvancedTrace.TraceInformation(string.Format("Debut  Traitement de la Version {0} et de son fichier source {1} ...", p.IdMultimedia.ToString(), p.FileName), ModuleName);
+                           string cFullSourceFilename = string.Format("{0}.avi", Path.Combine(p.FilePath, p.FileName));
+                           string cFullDestinationFileName = Path.Combine(TvDestinationPath, string.Format(@"{0}\", p.MediaYear.ToString()));
+
+                           bool isFileSourceExist = true;
+
+                           if (!File.Exists(cFullSourceFilename))
+                           {
+                               isFileSourceExist = false;
+                               notExistFilesSource.Add(string.Format("{0};{1}", p.IdMultimedia, cFullSourceFilename));
+
+                           };
+
+                           if (!Directory.Exists(cFullDestinationFileName))
+                               Directory.CreateDirectory(cFullDestinationFileName);
+                           
+                           if (!Directory.Exists(string.Format(@"{0}\360", cFullDestinationFileName)))                          
+                               Directory.CreateDirectory(string.Format(@"{0}\360", cFullDestinationFileName));
+                          
+                           if (!Directory.Exists(string.Format(@"{0}\240", cFullDestinationFileName)))                          
+                               Directory.CreateDirectory(string.Format(@"{0}\240", cFullDestinationFileName));
+                           
+                           string cHrFullDestinationFileName = string.Format("{0}.mp4", Path.Combine(cFullDestinationFileName, string.Format("3{0}",p.IdMultimedia.ToString())));                         
+                           string cMrFullDestinationFileName = string.Format("{0}.mp4", Path.Combine(cFullDestinationFileName, string.Format(@"360\3{0}", p.IdMultimedia.ToString())));
+                           string cBrFullDestinationFileName = string.Format("{0}.mp4", Path.Combine(cFullDestinationFileName, string.Format(@"240\3{0}", p.IdMultimedia.ToString())));
+
+                           bool isExistingDestFile = false;
+                           if (File.Exists(cHrFullDestinationFileName))
+                           {
+                               isExistingDestFile = true;
+                               filesExisting.Add(string.Format("{0};{1}", p.IdMultimedia, cHrFullDestinationFileName));
+
+                           };
+
+                           if (isFileSourceExist && !isExistingDestFile)
+                           {
+                               string tempAviFile = string.Format("{0}.avi", Path.Combine(temPath, p.FileName));
+                               string tempMp4File = string.Format("{0}.mp4", Path.Combine(temPath, string.Format("3{0}",p.IdMultimedia.ToString())));
+
+                               File.Copy(cFullSourceFilename, tempAviFile, true);
+
+                               //HR conversion
+                               var ffmpegLauncher = new FfmpegLauncher( string.Format(arguments,VideoHrQuality));
+                               bool isConverted = ffmpegLauncher.Convert(tempAviFile, tempMp4File);
+
+                               if (isConverted)
+                               {
+                                   File.Copy(tempMp4File, cHrFullDestinationFileName, true);
+                                   File.Delete(tempMp4File);                                 
+                                   reportGenerator.NbFileConverted++;
+                                   AdvancedTrace.TraceInformation(string.Format("La Version {0} et  son fichier source {1}  converti en MP4 et Haute resolution avec succès...", p.IdMultimedia.ToString(), p.FileName), ModuleName);
+                               }
+                               else
+                               {
+                                   AdvancedTrace.TraceInformation(string.Format("Echec conversion duc fichier {1} de la Version {0}  en MP4 et Haute resolution ...", p.IdMultimedia.ToString(), p.FileName), ModuleName);
+                                   filesNotConverted.Add(string.Format("{0};{1};{2};{3}<br/>", p.IdMultimedia, p.MediaYear, p.FileName, p.FilePath));
+                               }
+
+                               //MR conversion
+                                ffmpegLauncher = new FfmpegLauncher(string.Format(arguments, VideoMrQuality));
+                                isConverted = ffmpegLauncher.Convert(tempAviFile, tempMp4File);
+                                if (isConverted)
+                                {
+                                    File.Copy(tempMp4File, cMrFullDestinationFileName, true);
+                                    File.Delete(tempMp4File);
+                                    reportGenerator.NbFileConverted++;
+                                    AdvancedTrace.TraceInformation(string.Format("La Version {0} et  son fichier source {1}  converti en MP4 et Moyenne resolution avec succès...", p.IdMultimedia.ToString(), p.FileName), ModuleName);
+                                }
+                                else
+                                {
+                                    AdvancedTrace.TraceInformation(string.Format("Echec conversion duc fichier {1} de la Version {0}  en MP4 et Moyenne resolution ...", p.IdMultimedia.ToString(), p.FileName), ModuleName);
+                                    filesNotConverted.Add(string.Format("{0};{1};{2};{3}<br/>", p.IdMultimedia, p.MediaYear, p.FileName, p.FilePath));
+                                }
+
+                                //BR conversion
+                                ffmpegLauncher = new FfmpegLauncher(string.Format(arguments, VideoBrQuality));
+                                isConverted = ffmpegLauncher.Convert(tempAviFile, tempMp4File);
+                                if (isConverted)
+                                {
+                                    File.Copy(tempMp4File, cBrFullDestinationFileName, true);
+                                    File.Delete(tempMp4File);
+                                    reportGenerator.NbFileConverted++;
+                                    AdvancedTrace.TraceInformation(string.Format("La Version {0} et  son fichier source {1}  converti en MP4 et Basse resolution avec succès...", p.IdMultimedia.ToString(), p.FileName), ModuleName);
+                                }
+                                else
+                                {
+                                    AdvancedTrace.TraceInformation(string.Format("Echec conversion duc fichier {1} de la Version {0}  en MP4 et Basse resolution ...", p.IdMultimedia.ToString(), p.FileName), ModuleName);
+                                    filesNotConverted.Add(string.Format("{0};{1};{2};{3}<br/>", p.IdMultimedia, p.MediaYear, p.FileName, p.FilePath));
+                                }
+
+                               if(File.Exists(tempAviFile)) File.Delete(tempAviFile);
+                              
+                           }
+                       }
+
+                   });
+
+                   //Logging Source files not  existing
+                   if (notExistFilesSource.Any())
+                   {
+                       var dirNotExitFileSource = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"Logs\");
+                       if (!Directory.Exists(dirNotExitFileSource))
+                       {
+                           Directory.CreateDirectory(dirNotExitFileSource);
+                       }
+                       var notExitFileSourcePath = Path.Combine(dirNotExitFileSource, string.Format("TV_NotExitFileSource_{0}.txt", DateTime.Now.ToString("yyyyMMddHHmmss")));
+                       File.AppendAllLines(notExitFileSourcePath, notExistFilesSource, Encoding.ASCII);
+                       reportGenerator.NbFilesSourceNotExisting = notExistFilesSource.Count();
+                   }
+
+
+                   //Logging Nb files not converted
+                   if (filesNotConverted.Any())
+                   {
+                       var dirFilesNotConverted = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"Logs\");
+                       if (!Directory.Exists(dirFilesNotConverted))
+                       {
+                           Directory.CreateDirectory(dirFilesNotConverted);
+                       }
+                       var filesNotConvertedPath = Path.Combine(dirFilesNotConverted, string.Format("TV_FilesNotConverted_{0}.txt", DateTime.Now.ToString("yyyyMMddHHmmss")));
+                       File.AppendAllLines(filesNotConvertedPath, filesNotConverted, Encoding.ASCII);
+                       reportGenerator.NbFileConverted = filesNotConverted.Count();
+                   }
+
+                   //Logging Nb existing destination file
+                   if (filesExisting.Any())
+                   {
+                       var dirFilesExisting = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"Logs\");
+                       if (!Directory.Exists(dirFilesExisting))
+                       {
+                           Directory.CreateDirectory(dirFilesExisting);
+                       }
+                       var path = Path.Combine(dirFilesExisting, string.Format("TV_FilesDestinationExisting_{0}.txt", DateTime.Now.ToString("yyyyMMddHHmmss")));
+                       File.AppendAllLines(path, filesExisting, Encoding.ASCII);
+                       reportGenerator.NbExistingDestinationFiles = filesExisting.Count();
+                   }   
+
+                   if (Directory.Exists(temPath)) Directory.Delete(temPath, true);
+                   filesNotConverted = null;
+                   notExistFilesSource = null;
+                   AdvancedTrace.TraceInformation(" End of Video treatment ...", ModuleName);
+                   SendEmail(reportGenerator.GetHtml());
+               }
+               ReleaseTask(taskExecution);
+           }
+           catch (Exception exception)
+           {
+               ReleaseTask(taskExecution,
+                         new LogLine("An error occured during Audio Conversion treatment.", exception,
+                                     eLogCategories.Fatal));
+
+           }
+           finally
+           {
+               if (UseImpersonate && _impersonateInformation != null) CloseImpersonation();
+               AdvancedTrace.TraceInformation(string.Format("{0} is done.", taskLogHeader),
+                                            ModuleName);
+           }
+       }
+    }
+}
---
+++ AresService/KM.AdExpress.AudioVideoConverter/Engine.cs
@@ -0,0 +1,267 @@
+﻿using KM.AdExpress.AudioVideoConverter.DAL;
+using KM.AdExpress.AudioVideoConverter.Domain;
+using KM.AdExpress.AudioVideoConverter.Domain.XmlLoaders;
+using KM.AdExpress.AudioVideoConverter.Tools;
+using LinkSystem.LinkClient;
+using LinkSystem.LinkKernel.Core;
+using LinkSystem.LinkKernel.CoreMonitor;
+using LinkSystem.LinkKernel.Interfaces;
+using LinkSystem.LinkMonitor;
+using LinkSystem.LinkMonitor.AdvancedTraceListeners;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace KM.AdExpress.AudioVideoConverter
+{
+    public partial class Engine : LinkClient, IServer_SelectRule
+    {
+        #region Constants
+        private const string ConfigurationDirectoryName = "Configuration";
+        private const int Timeout = 10000;
+        private const string ErrorMailFileName = "ErrorMail.xml";
+        private const string LinkClientFileName = "LinkClient.xml";
+        private const string LsClientConfigurationFileName = "LsClientConfiguration.xml";     
+        readonly string _configurationDirectoryRoot = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, ConfigurationDirectoryName);
+        const string ModuleName = "ModuleName";
+        #endregion
+
+        #region Variable
+
+        /// <summary>
+        /// Modules
+        /// </summary>
+        private readonly List<ModuleDescription> _modules;
+
+        /// <summary>
+        /// Server link
+        /// </summary>
+        private readonly ServerLink _serverLink;
+
+        /// <summary>
+        /// Impersonate Information
+        /// </summary>
+        private ImpersonateInformation _impersonateInformation = null;
+        /// <summary>
+        /// Impersonation
+        /// </summary>
+        private Impersonation _oImp = null;
+
+        private bool _isSelected;
+        private MonitorServer _oMonitorServer;
+
+       // private List<PreRollInformation> _preRollInformations;
+        private string _connectionString = string.Empty;
+        private string _providerDataAccess = string.Empty;
+        private string _customerMailFrom = string.Empty;      
+        private string _customerMailServer = string.Empty;
+        public string RadioDestinationPath { get; set; }
+        public string TvDestinationPath { get; set; }
+        public string VideoBrQuality { get; set; }
+        public string VideoMrQuality { get; set; }
+        public string VideoHrQuality { get; set; }
+
+        public bool UseImpersonate { get; set; }
+        #endregion
+
+        #region Constructor / Destructor
+
+        public Engine()
+        {
+
+            var productInfo = ProductInformationLoader.Load(Path.Combine(_configurationDirectoryRoot, LsClientConfigurationFileName));
+
+            // MonitorServer
+            _oMonitorServer = new MonitorServer();
+            _oMonitorServer.ActivateDiskPersistence(productInfo.ProductInformation.ProductName, 7);
+            _oMonitorServer.ActivateCommunication(productInfo.MonitorPort);
+
+
+            // Configure the advanced trace
+            AdvancedTrace.RegisterTraceListenerToAllTraceType(new ConsoleStandardOutputListener());
+            AdvancedTrace.RegisterTraceListenerToAllTraceType(new MonitorServerListener(_oMonitorServer));
+            AdvancedTrace.RegisterCustomTraceType(ModuleName);
+
+
+            // MailerConfig
+            var configEmailFileName = Path.Combine(_configurationDirectoryRoot, ErrorMailFileName);
+            if (File.Exists(configEmailFileName))
+            {
+                var mailerConfig = MailerConfigLoader.Load(configEmailFileName);
+
+                _oMonitorServer.ActivateMailer(mailerConfig);
+
+                // AdvancedTrace.RegisterTraceListener(TraceTypeConstant.Warning, new MonitorServerEmailListener(_oMonitorServer));
+                AdvancedTrace.RegisterTraceListener(TraceTypeConstant.Error, new MonitorServerEmailListener(_oMonitorServer));
+                AdvancedTrace.RegisterTraceListener(TraceTypeConstant.Fatal, new MonitorServerEmailListener(_oMonitorServer));
+            }
+
+            // Load modules
+            _modules = ModuleDescriptionLoader.Load(Path.Combine(_configurationDirectoryRoot, LsClientConfigurationFileName));
+
+            // Load LinkClient.xml configuration and connect to the link server
+            _serverLink = ServerLinkConfigurationLoader.Load(Path.Combine(_configurationDirectoryRoot, LinkClientFileName));
+
+            CallTimeout = Timeout;
+            AlwaysReconnect = true;
+
+
+            _providerDataAccess = ConfigurationManager.AppSettings["ProviderDataAccess"];          
+            _connectionString =
+                String.Format(
+                    Provider.GetConnectionStringByProvider(_providerDataAccess),
+                    ConfigurationManager.AppSettings["DbUserName"],
+                    ConfigurationManager.AppSettings["DbPassword"],
+                    ConfigurationManager.AppSettings["DbDataSource"]);
+
+            RadioDestinationPath = ConfigurationManager.AppSettings["RadioDestinationPath"];
+            TvDestinationPath = ConfigurationManager.AppSettings["TvDestinationPath"];
+
+            VideoBrQuality = ConfigurationManager.AppSettings["VideoBrQuality"];
+            VideoMrQuality = ConfigurationManager.AppSettings["VideoMrQuality"];
+            VideoHrQuality = ConfigurationManager.AppSettings["VideoHrQuality"];
+
+            UseImpersonate = Convert.ToBoolean(ConfigurationManager.AppSettings["UseImpersonate"]);
+            _impersonateInformation = new ImpersonateInformation(ConfigurationManager.AppSettings["User"],
+                ConfigurationManager.AppSettings["Password"], ConfigurationManager.AppSettings["Domain"]);
+            try
+            {
+                Connect(_serverLink.Host, _serverLink.Port, true);
+
+                _isSelected = true;
+            }
+            catch (Exception exception)
+            {
+                AdvancedTrace.TraceFatal("The configuration file contains errors.", exception);
+            }
+        }
+
+        public override void Dispose()
+        {
+            CancelAllTasks();
+            WaitForRunningTaskCompletion();
+
+            _oMonitorServer.Dispose();
+            _oMonitorServer = null;
+
+            base.Dispose();
+        }
+
+        #endregion
+
+        #region Overrides of LinkClient
+
+        protected override ProductInformation RetrieveProductInformation()
+        {
+            return ProductInformationLoader.Load(Path.Combine(_configurationDirectoryRoot, LsClientConfigurationFileName)).ProductInformation;
+        }
+
+        #endregion
+
+        #region Tasks management
+
+        protected override void OnTaskExecute(TaskExecution taskExecution)
+        {
+            switch (taskExecution.ModuleID)
+            {
+                case 43:
+                    ExecuteTask(taskExecution, DoAudioConversion, Guid.NewGuid().ToString());
+                    break;
+                case 44:
+                    ExecuteTask(taskExecution, DoVideoConversion, Guid.NewGuid().ToString());
+                    break;
+            }
+        }
+
+        #endregion
+
+        #region Implement IServer_SelectRule
+
+        public void Block(bool bln)
+        {
+            _isSelected = !bln;
+        }
+
+        public void BlockModule(int intModuleID, bool bln)
+        {
+            _modules.First(p => p.ModuleID == intModuleID).IsSelectable = bln;
+        }
+
+        public bool IsSelectable()
+        {
+            return _isSelected;
+        }
+
+        public List<ModuleDescription> KnownModules()
+        {
+            return _modules;
+        }
+
+        public int MaxAvailableSlots()
+        {
+            return 1;
+        }
+
+        #endregion
+
+        #region Private Methos
+
+        private void SendEmail(String message)
+        {
+            var oMessage = new MonitorMailMessage
+            {
+                Subject = String.Format("AdExpress Audio Video Converter > KM.AdExpress.AudioVideoConverter.App :  Audio Video Converter SUCCESSFUL"),
+                Title = "AdExpress",
+            };
+
+            oMessage.AddHTMLLine(message);
+
+            _oMonitorServer.SendEmail(oMessage);
+        }
+
+        public static void SendEmail(MonitorServer oMonitorServer, String message, String appName, String moduleName, Boolean isInError = false)
+        {
+            var oMessage = new MonitorMailMessage
+            {
+                Subject = String.Format("AdExpress > {0} : {1} {2}", appName, moduleName, isInError ? "ERROR" : "SUCCESSFUL"),
+                Title = "AdExpress ",
+            };
+
+            oMessage.AddHTMLLine(message);
+
+            oMonitorServer.SendEmail(oMessage);
+        }
+        #endregion
+
+        #region Impersonation Methods
+        /// <summary>
+        /// Open Impersonation
+        /// </summary>
+        /// <returns></returns>
+        public void OpenImpersonation()
+        {
+
+            if (_impersonateInformation != null)
+            {
+                CloseImpersonation();
+                _oImp = new Impersonation();
+                _oImp.ImpersonateValidUser(_impersonateInformation.UserName, _impersonateInformation.Domain, _impersonateInformation.Password, Impersonation.LogonType.LOGON32_LOGON_NEW_CREDENTIALS);
+            }
+        }
+        /// <summary>
+        /// Close Impersonation
+        /// </summary>
+        public void CloseImpersonation()
+        {
+            if (_oImp != null)
+                _oImp.UndoImpersonation();
+            _oImp = null;
+        }
+        #endregion
+
+       
+    }
+}
