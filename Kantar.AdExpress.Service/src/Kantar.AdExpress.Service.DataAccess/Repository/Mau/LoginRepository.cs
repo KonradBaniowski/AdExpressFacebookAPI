3af7d59e82e1586ba7330323410c9dc0246e4303
CITnDev <citndev@gmail.com>
2016-01-28 18:00:45 +0100

    Compilable sur le PC de seb (nécessité d'installer odp.net x64 via l'installer)

--- Kantar.AdExpress.Service/src/Kantar.AdExpress.Service.DataAccess/Repository/Mau/LoginRepository.cs
+++ Kantar.AdExpress.Service/src/Kantar.AdExpress.Service.DataAccess/Repository/Mau/LoginRepository.cs
@@ -17,19 +17,13 @@ namespace Kantar.AdExpress.Service.DataAccess.Repository.Mau
 
         public async Task<Login> GetLogin(string name)
         {
-
-            var res = (from a in mycontext.Login
-                       where a.LoginName == name.ToUpper()
-                       select a).FirstOrDefault();
-            return res;
-            /*
             return await Task.Run(() =>
             {
                 var res = (from a in mycontext.Login
                            where a.LoginName == name.ToUpper()
                            select a).FirstOrDefault();
                 return res;
-            });*/
+            });
         }
 
         public async Task<bool> CheckPasswordAsync(string userName, string password)
