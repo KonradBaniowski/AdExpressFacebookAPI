03fb2af588fb5ed3183d1597d17a107e528e8cef
savoury <wrap.romain@gmail.com>
2016-02-09 13:42:43 +0100

    add ViewModel

---
+++ Kantar.AdExpress.Service/src/Km.AdExpressClientWeb/Models/HomePageViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Km.AdExpressClientWeb.Models
+{
+    public class HomePageViewModel
+    {
+        public Dictionary<Int64, Module> ModuleRight { get; set; }
+        
+    }
+    public class Module
+    {
+
+    }
+}
