6f6f208423ed57492da01c7bd8770e46f315991d
savoury <wrap.romain@gmail.com>
2016-02-10 13:48:10 +0100

    Media Client

--- Kantar.AdExpress.Service/src/Km.AdExpressClientWeb/Controllers/MediaScheduleController.cs
+++ Kantar.AdExpress.Service/src/Km.AdExpressClientWeb/Controllers/MediaScheduleController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Km.AdExpressClientWeb.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -20,7 +21,66 @@ namespace Km.AdExpressClientWeb.Controllers
 
         public ActionResult MediaSelection()
         {
-            return View();
+            var model = new MediaSelectionViewModel()
+            {
+                Multiple = true,
+                Medias = new List<Media>()
+                {
+                    new Media()
+                    {
+                        Id = 1,
+                        Label = "Nom 1",
+                        Disabled = false
+                    },
+                    new Media()
+                    {
+                        Id = 3,
+                        Label = "Nom 3",
+                        Disabled = false
+                    },
+                    new Media()
+                    {
+                        Id = 2,
+                        Label = "Nom 2",
+                        Disabled = true
+                    },
+                        new Media()
+                    {
+                        Id = 4,
+                        Label = "Nom 4",
+                        Disabled = false
+                    },
+                            new Media()
+                    {
+                        Id = 5,
+                        Label = "Nom 5",
+                        Disabled = false
+                    },
+                      new Media()
+                    {
+                        Id = 6,
+                        Label = "Nom 6",
+                        Disabled = false
+                    },
+                          new Media()
+                    {
+                        Id = 7,
+                        Label = "Nom 7",
+                        Disabled = true
+                    }
+
+                },
+                IdMediasCommon =
+                new List<int>()
+                {
+                    { 1 },
+                    { 2 },
+                    { 4 },
+                    { 6 }
+                }
+            };
+            model.Medias = model.Medias.OrderBy(ze => ze.Disabled).ToList();
+            return View(model);
         }
 
         public ActionResult PeriodSelection()
---
+++ Kantar.AdExpress.Service/src/Km.AdExpressClientWeb/Models/MediaScheduleViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Km.AdExpressClientWeb.Models
+{
+    public class MediaSelectionViewModel
+    {
+        public List<Media> Medias { get; set; }
+        public bool Multiple { get; set; }
+        public List<int> IdMediasCommon { get; set; }
+
+    }
+
+    public class Media
+    {
+        public int Id { get; set; }
+        public string Label { get; set; }
+        public bool Disabled { get; set; }
+    }
+}
