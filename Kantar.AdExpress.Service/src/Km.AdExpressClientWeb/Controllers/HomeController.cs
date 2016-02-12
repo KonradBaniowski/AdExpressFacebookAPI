a67bd27fd8657ca9056f9c888605681a5856b296
savoury <wrap.romain@gmail.com>
2016-02-12 16:55:33 +0100

    Update Route

--- Kantar.AdExpress.Service/src/Km.AdExpressClientWeb/Controllers/HomeController.cs
+++ Kantar.AdExpress.Service/src/Km.AdExpressClientWeb/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using Km.AdExpressClientWeb.Models;
+using System.Collections.Generic;
 using System.Web.Mvc;
 
 namespace Km.AdExpressClientWeb.Controllers
@@ -8,13 +9,78 @@ namespace Km.AdExpressClientWeb.Controllers
     {
         public ActionResult Index()
         {
+            var docu = new Documents()
+            {
+                Id = 1,
+                Label = "AdExpress News",
+                InfosNews = new List<InfosNews>()
+                {
+                    new InfosNews()
+                    {
+                        Label = "Novembre 2015",
+                        Url = "/AdExNews/AdexNews_201511.pdf"
+                    },
+                    new InfosNews()
+                    {
+                        Label = "Octobre 2015",
+                        Url = "/AdExNews/AdexNews_201510.pdf"
+                    }
+                }
+            };
+
             var model = new HomePageViewModel()
             {
                 ModuleRight = new System.Collections.Generic.Dictionary<long, Module>()
                 {
-                    { 196, new Module() },
+                    { 198, new Module() },
                     { 197, new Module() },
-                    { 198, new Module() }
+                    { 7216, new Module() },
+                    { 1781, new Module() },
+                    { 4370, new Module() }
+                },
+                Documents = new List<Documents>() {
+                    new Documents()
+                    {
+                        Id = 1,
+                        Label = "AdExpress News",
+                        InfosNews = new List<InfosNews>()
+                        {
+                            new InfosNews()
+                            {
+                                Label = "Novembre 2015",
+                                Url = "/AdExNews/AdexNews_201511.pdf"
+                            },
+                            new InfosNews()
+                            {
+                                Label = "Octobre 2015",
+                                Url = "/AdExNews/AdexNews_201510.pdf"
+                            }
+                        }
+                    },
+                    new Documents()
+                    {
+                        Id = 2,
+                        Label = "AdExpress Report",
+                        InfosNews = new List<InfosNews>()
+                        {
+                            new InfosNews()
+                            {
+                                Label = "Novembre 2015",
+                                Url = "/AdExReport/AdExReport_201511.pdf"
+                            },
+                            new InfosNews()
+                            {
+                                Label = "Octobre 2015",
+                                Url = "/AdExReport/AdExReport_201510.pdf"
+                            }
+                        }
+                    },
+                     new Documents()
+                    {
+                        Id = 3,
+                        Label = "Documents",
+                        InfosNews = null
+                    }
                 }
             };
             return View(model);
--- Kantar.AdExpress.Service/src/Km.AdExpressClientWeb/Models/HomePageViewModel.cs
+++ Kantar.AdExpress.Service/src/Km.AdExpressClientWeb/Models/HomePageViewModel.cs
@@ -8,10 +8,25 @@ namespace Km.AdExpressClientWeb.Models
     public class HomePageViewModel
     {
         public Dictionary<Int64, Module> ModuleRight { get; set; }
+        public List<Documents> Documents { get; set; }
         
     }
     public class Module
     {
 
+    }
+
+    public class Documents
+    {
+        public int Id { get; set; }
+        public string Label { get; set; }
+
+        public List<InfosNews> InfosNews { get; set; }
+    }
+
+    public class InfosNews
+    {
+        public string Url { get; set; }
+        public string Label { get; set; }
     }
 }
