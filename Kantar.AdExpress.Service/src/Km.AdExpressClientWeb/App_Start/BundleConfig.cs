5c26c7874bd720f63c0b3e89a9c0ae8a228b4423
dedson <dede.mussuma@kantarmedia.com>
2016-02-19 18:01:30 +0100

    ajout Ignite ui

--- Kantar.AdExpress.Service/src/Km.AdExpressClientWeb/App_Start/BundleConfig.cs
+++ Kantar.AdExpress.Service/src/Km.AdExpressClientWeb/App_Start/BundleConfig.cs
@@ -8,7 +8,9 @@ namespace Km.AdExpressClientWeb
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jquery-ui").Include(
                         "~/Scripts/jquery-ui-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
