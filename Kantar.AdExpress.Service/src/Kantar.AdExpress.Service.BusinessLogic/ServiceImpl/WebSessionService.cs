22bb182660f1ac793b97f8c774305a0ecb2aee65
SAAZ <s.allah.azzaza@gmail.com>
2016-02-22 13:12:56 +0100

    add

---
+++ Kantar.AdExpress.Service/src/Kantar.AdExpress.Service.BusinessLogic/ServiceImpl/WebSessionService.cs
@@ -0,0 +1,17 @@
+﻿using Kantar.AdExpress.Service.Core.BusinessService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kantar.AdExpress.Service.BusinessLogic.ServiceImpl
+{
+    class WebSessionService : IWebSessionService
+    {
+        public void SaveMediaSelection (List<int> mediaIds, string webSessionId)
+        {
+
+        }
+    }
+}
