6ac9769561a22c722f07cbb7fad0330f548f2901
savoury <wrap.romain@gmail.com>
2016-02-24 18:13:28 +0100

    Debut univers Service

---
+++ Kantar.AdExpress.Service/src/Kantar.AdExpress.Service.BusinessLogic/ServiceImpl/UniverseService.cs
@@ -0,0 +1,20 @@
+﻿using Kantar.AdExpress.Service.Core.BusinessService;
+using Kantar.AdExpress.Service.Core.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kantar.AdExpress.Service.BusinessLogic.ServiceImpl
+{
+    public class UniverseService : IUniverseService
+    {
+        public List<UniverseItem> GetItems(int universeLevelId, string keyWord)
+        {
+            return new List<UniverseItem>();
+        }
+
+
+    }
+}
---
+++ Kantar.AdExpress.Service/src/Kantar.AdExpress.Service.Core/Domain/UniverseItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kantar.AdExpress.Service.Core.Domain
+{
+    public class UniverseItem
+    {
+        public int IdItem { get; set; }
+        public string Label { get; set; }
+        //TODO USEFULL?
+        public int IdLevelUniverse { get; set; }
+    }
+}
