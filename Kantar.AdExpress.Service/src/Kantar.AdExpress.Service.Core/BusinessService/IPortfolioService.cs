a82b5c2cd9c3ae41eeece856dae9cad11a0ab24b
dedson <dede.mussuma@kantarmedia.com>
2016-03-01 10:49:17 +0100

    Ajout du construteur dans PortfolioController

--- Kantar.AdExpress.Service/src/Kantar.AdExpress.Service.Core/BusinessService/IPortfolioService.cs
+++ Kantar.AdExpress.Service/src/Kantar.AdExpress.Service.Core/BusinessService/IPortfolioService.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace Kantar.AdExpress.Service.Core.BusinessService
 {
     public interface IPortfolioService
--- Kantar.AdExpress.Service/src/Km.AdExpressClientWeb/Controllers/PortfolioController.cs
+++ Kantar.AdExpress.Service/src/Km.AdExpressClientWeb/Controllers/PortfolioController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Kantar.AdExpress.Service.Core.BusinessService;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,8 +7,21 @@ using System.Web.Mvc;
 
 namespace Km.AdExpressClientWeb.Controllers
 {
+    [Authorize]
     public class PortfolioController : Controller
     {
+        private IPortfolioService _portoFolioService;
+        private IMediaService _mediaService;
+        private IWebSessionService _webSessionService;
+        private IUniverseService _universeService;
+        private const string _controller = "Portfolio";
+        public PortfolioController(IPortfolioService portofolioService, IMediaService mediaService, IWebSessionService webSessionService, IUniverseService universeService)
+        {
+            _portoFolioService = portofolioService;
+            _mediaService = mediaService;
+            _webSessionService = webSessionService;
+            _universeService = universeService;
+        }
         // GET: Portfolio
         public ActionResult Index()
         {
