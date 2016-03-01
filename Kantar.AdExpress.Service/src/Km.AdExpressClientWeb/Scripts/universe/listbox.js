deec9d11481b1056da0afd9c13b1e929d178fe82
savoury <wrap.romain@gmail.com>
2016-03-01 09:35:41 +0100

    marche selection

--- Kantar.AdExpress.Service/src/Km.AdExpressClientWeb/Scripts/universe/listbox.js
+++ Kantar.AdExpress.Service/src/Km.AdExpressClientWeb/Scripts/universe/listbox.js
@@ -9,10 +9,9 @@ function GetHtmlSelectableFromObject(datas, id, className){
 				grp.addClass(className);
 			for(var i=0; i<datas.length; i++){
 				var item = $('<li/>');
-				item.addClass('ui-widget-content');
-				item.val(datas[i].Id);
-				item.text(datas[i].Text);
-				
+				item.val(datas[i].IdItem);
+				item.attr('data-id', datas[i].IdItem)
+				item.text(datas[i].Label);
 				item.appendTo(grp);
 			}
 			return grp;
@@ -31,13 +30,12 @@ function GetHtmlSelectableFromObject(datas, id, className){
 				this.html('');
 		},
 		fillGroupSelectable:function(titleText, datas, classNameTitle, classNameDivSelection, idSelection, classNameSelection, nbElemMax, nbElemMaxText) {
-
 			var divHtmlList = $('<div/>');
 			if(classNameDivSelection!=undefined)
 				divHtmlList.addClass(classNameDivSelection);
 			
 			var nbElemHtml = $('<span/>');
-
+			nbElemHtml.addClass('badge bg-blue');
 			var listElem = undefined;
 			var warningHtml = undefined;
 			if(datas.length>nbElemMax)
@@ -57,9 +55,11 @@ function GetHtmlSelectableFromObject(datas, id, className){
 			titleText = titleText.replace('{NB_ELEM}', $("<div>").append(nbElemHtml.clone()).html());
 			
 			var divTitleHtml = $('<div/>');
+			var headerTitle = $('<h6/>');
+
 			if(classNameTitle!=undefined)
 				divTitleHtml.addClass(classNameTitle);
-			divTitleHtml.append(titleText);
+			divTitleHtml.append(headerTitle.addClass('famille blue uppercase').append(titleText));
 			
 			if(warningHtml!=undefined)
 				divTitleHtml.append(warningHtml);
--- Kantar.AdExpress.Service/src/Km.AdExpressClientWeb/Scripts/universe/universe.js
+++ Kantar.AdExpress.Service/src/Km.AdExpressClientWeb/Scripts/universe/universe.js
@@ -1,65 +1,53 @@
-﻿function BuildJson(title, nbElem) {
-    var jsonResult = '[{"Title":"' + title + '","Values":[';
-    for (var i = 0 ; i < nbElem ; i++) {
-        if (i > 0)
-            jsonResult += ',';
-        jsonResult += '{"Id":' + i + ', "Text": "Item ' + i + '"}';
-    }
-    jsonResult += ']}]';
-
-    return jsonResult;
-}
-
+﻿
 $(function () {
-
-
-    var jsonResult1 = BuildJson('Groupe de sociétés 1 \{NB_ELEM\}', 35);
-    //var jsonResult2 = BuildJson('Groupe de sociétés 2 \{NB_ELEM\}', 35);
-    //var jsonResult3 = BuildJson('Groupe de sociétés 3 \{NB_ELEM\}', 1001);
-
-
-    var jsonParse1 = JSON.parse(jsonResult1)[0];
-
-    $("#groupSelectable1").fillGroupSelectable(jsonParse1.Title, jsonParse1.Values, 'panel-heading', 'panel-body', 'containerSelectable1', undefined, 1000, '{NB_ELEM_MAX} éléments sur {NB_ELEM}. Affinez votre recherche.')
-
-    $("#containerSelectable1").selectable(
-    {
-        stop: function () {
-            ShowSelection($(this));
-        }
+    $(".dropdown-menu.bg-blue.pull-right li > a").on('click', function (e) {
+        e.preventDefault();
+        var selText = $(this).text();
+        var selValue = $(this).attr("data-id");
+        //$("#" + selValue).show();
+        $(this).parents('.input-group-btn').find('.btn.btn-default.select-recherche').html(selText + '<span class="caret"></span>');
     });
 
-    //var jsonParse2 = JSON.parse(jsonResult2)[0];
+    $(".btn-recherche").on('click', function (event) {
+        var keyword = $('#keyword').val();
+        var branchId = $('#branch').attr("data-branch");
+        $(".universes").hide();
+        $("#branch" + branchId).show();
+        $("#branch" + branchId + "> div").html('');
+        $("#branch" + branchId + "> div").each(function () {
+            var univerIndex = parseFloat($(this).attr('data-universe'))
+            var univerLabel = $(this).attr('data-label') + "\{NB_ELEM\}";
+            var params = {
+                keyWord: keyword,
+                universeId: univerIndex
+            };
+            $.ajax({
+                url: '/Universe/GetUniverses',
+                contentType: 'application/json',
+                type: 'POST',
+                datatype: 'JSON',
+                data: JSON.stringify(params),
+                error: function (xmlHttpRequest, errorText, thrownError) {
+                    alert("error");
+                },
+                success: function (response) {
+                    $("#groupSelectable" + univerIndex).fillGroupSelectable(univerLabel, response.data, 'panel-heading', 'panel-body', 'containerSelectable1', undefined, 1000, '{NB_ELEM_MAX} éléments sur {NB_ELEM}. Affinez votre recherche.');
+                    $("#groupSelectable" + univerIndex).selectable(
+                    {
+                    });
+                }
+            });
+        });
+    });
 
-    //$("#groupSelectable2").fillGroupSelectable(jsonParse2.Title, jsonParse2.Values, 'panel-heading', 'panel-body', 'containerSelectable2', undefined, 1000, '{NB_ELEM_MAX} éléments sur {NB_ELEM}. Affinez votre recherche.')
-
-    //$("#containerSelectable2").selectable(
-    //{
-    //    stop: function () {
-    //        ShowSelection($(this));
-    //    }
-    //});
-
-    //var jsonParse3 = JSON.parse(jsonResult3)[0];
-
-    //$("#groupSelectable3").fillGroupSelectable(jsonParse3.Title, jsonParse3.Values, 'panel-heading', 'panel-body', 'containerSelectable3', undefined, 1000, '{NB_ELEM_MAX} éléments sur {NB_ELEM}. Affinez votre recherche.')
-
-    //$("#containerSelectable3").selectable(
-    //{
-    //    stop: function () {
-    //        ShowSelection($(this));
-    //    }
-    //});
 
 });
 
 function ShowSelection(elem) {
     var selectedItems = elem.getSelectableSelectedItems();
-
     var result = '';
     selectedItems.forEach(function (item) {
         result += ' #' + item.Value + ';' + item.Text + '\n';
     });
-
-    alert('Selection : \n' + result);
 }
+
