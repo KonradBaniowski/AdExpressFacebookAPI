4c209d851e551b0457fbdd21722316d074148df3
dedson <dede.mussuma@kantarmedia.com>
2016-03-03 09:28:49 +0100

    Ajout nde la classe PeriodResponse dans Kantar.AdExpress.Service.Core

---
+++ Kantar.AdExpress.Service/src/Kantar.AdExpress.Service.Core/Domain/Period.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kantar.AdExpress.Service.Core.Domain
+{
+    public class PeriodResponse
+    {
+        public int StartYear { get; set; }
+        public int SiteLanguage { get; set; }
+    }
+}
