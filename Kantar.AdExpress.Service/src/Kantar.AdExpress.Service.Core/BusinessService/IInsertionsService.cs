a69db82d417d9d0d9324b894577fabf4b61e69bc
dedson <dede.mussuma@kantarmedia.com>
2016-03-06 19:35:22 +0100

    Ajout de l'interface IInsertionsService

---
+++ Kantar.AdExpress.Service/src/Kantar.AdExpress.Service.Core/BusinessService/IInsertionsService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kantar.AdExpress.Service.Core.BusinessService
+{
+    interface IInsertionsService
+    {
+    }
+}
