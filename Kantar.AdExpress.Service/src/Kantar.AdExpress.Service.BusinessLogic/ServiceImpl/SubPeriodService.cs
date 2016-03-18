d57874f7c1eb5fd58bf68b06908f5108a6cda12e
yrkaina <youssef.rkaina@gmail.com>
2016-03-18 12:44:33 +0100

    Add sub period control for Media Schedule

---
+++ Kantar.AdExpress.Service/src/Kantar.AdExpress.Service.BusinessLogic/ServiceImpl/SubPeriodService.cs
@@ -0,0 +1,179 @@
+﻿using Kantar.AdExpress.Service.Core.BusinessService;
+using Kantar.AdExpress.Service.Core.Domain;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TNS.AdExpress.Domain.Translation;
+using TNS.AdExpress.Domain.Web;
+using TNS.AdExpress.Web.Core.Sessions;
+using TNS.AdExpress.Web.Core.Utilities;
+using TNS.FrameWork.Date;
+using WebCst = TNS.AdExpress.Constantes.Web;
+
+namespace Kantar.AdExpress.Service.BusinessLogic.ServiceImpl
+{
+    public class SubPeriodService : ISubPeriodService
+    {
+        public SubPeriod GetSubPeriod(string idWebSession, string zoomDate)
+        {
+            SubPeriod subPeriod = new SubPeriod();
+
+            WebSession CustomerSession = (WebSession)WebSession.Load(idWebSession);
+
+            int periodIndex = 0;
+            int i = -1;
+            string labBegin = CustomerSession.PeriodBeginningDate;
+            string labEnd = CustomerSession.PeriodEndDate;
+            string themeName = WebApplicationParameters.Themes[CustomerSession.SiteLanguage].Name;
+
+            CultureInfo cultureInfo = new CultureInfo(WebApplicationParameters.AllowedLanguages[CustomerSession.SiteLanguage].Localization);
+            IFormatProvider fp = WebApplicationParameters.AllowedLanguages[CustomerSession.SiteLanguage].CultureInfo;
+
+            WebCst.CustomerSessions.Period.Type periodType = CustomerSession.PeriodType;
+            WebCst.CustomerSessions.Period.DisplayLevel periodDisplay = CustomerSession.DetailPeriod;
+            string periodBegin = CustomerSession.PeriodBeginningDate;
+            string periodEnd = CustomerSession.PeriodEndDate;
+            string realPeriodBegin = CustomerSession.PeriodBeginningDate;
+            string realPeriodEnd = CustomerSession.PeriodEndDate;
+
+            DateTime begin = Dates.getPeriodBeginningDate(realPeriodBegin, CustomerSession.PeriodType);
+            DateTime today = DateTime.Now.Date;
+
+            realPeriodBegin = begin.ToString("yyyyMMdd");
+            realPeriodEnd = Dates.getPeriodEndDate(realPeriodEnd, CustomerSession.PeriodType).ToString("yyyyMMdd");
+
+            if (periodDisplay == WebCst.CustomerSessions.Period.DisplayLevel.weekly)
+            {
+                periodType = WebCst.CustomerSessions.Period.Type.dateToDateWeek;
+                var tmp = new AtomicPeriodWeek(new DateTime(int.Parse(realPeriodBegin.Substring(0, 4)), int.Parse(realPeriodBegin.Substring(4, 2)), int.Parse(realPeriodBegin.Substring(6, 2))));
+                periodBegin = string.Format("{0}{1}", tmp.FirstDay.AddDays(3).Year, tmp.Week.ToString("0#"));
+                tmp = new AtomicPeriodWeek(new DateTime(int.Parse(realPeriodEnd.Substring(0, 4)), int.Parse(realPeriodEnd.Substring(4, 2)), int.Parse(realPeriodEnd.Substring(6, 2))));
+                periodEnd = string.Format("{0}{1}", tmp.FirstDay.AddDays(3).Year, tmp.Week.ToString("0#"));
+                subPeriod.PeriodLabel = "Semaine";
+            }
+            else
+            {
+                periodType = WebCst.CustomerSessions.Period.Type.dateToDateMonth;
+                periodBegin = realPeriodBegin.Substring(0, 6);
+                periodEnd = realPeriodEnd.Substring(0, 6);
+                subPeriod.PeriodLabel = "Mois";
+            }
+
+            string currentPeriod = periodBegin;
+            periodIndex = -1;
+            i = -1;
+            List<string> activePeriods = null;
+
+            subPeriod.Items = new List<SubPeriodItem>();
+            SubPeriodItem item = new SubPeriodItem();
+
+            do
+            {
+                i++;
+
+                item = new SubPeriodItem();
+
+                item.Period = currentPeriod;
+
+                if (periodDisplay == WebCst.CustomerSessions.Period.DisplayLevel.weekly)
+                    item.Label = currentPeriod.Substring(4, 2);
+                else
+                    item.Label = MonthString.GetCharacters(int.Parse(currentPeriod.Substring(4, 2)), cultureInfo, 1);
+
+                AppendPeriod(CustomerSession, periodType, currentPeriod, i, ref item, ref periodIndex, realPeriodBegin, realPeriodEnd, ref labBegin, ref labEnd, activePeriods);
+
+                if (item.Period == zoomDate)
+                {
+                    item.IsSelected = true;
+                    subPeriod.FirstPeriodLabel = item.PeriodLabel;
+                }
+                else
+                    item.IsSelected = false;
+
+                subPeriod.Items.Add(item);
+
+                if (periodType != WebCst.CustomerSessions.Period.Type.dateToDateWeek)
+                {
+                    currentPeriod = (new DateTime(int.Parse(currentPeriod.Substring(0, 4)), int.Parse(currentPeriod.Substring(4, 2)), 1)).AddMonths(1).ToString("yyyyMM");
+                }
+                else
+                {
+                    var tmp = new AtomicPeriodWeek(int.Parse(currentPeriod.Substring(0, 4)), int.Parse(currentPeriod.Substring(4, 2)));
+                    tmp.Increment();
+                    currentPeriod = string.Format("{0}{1}", tmp.Year, tmp.Week.ToString("0#"));
+                }
+
+            } while (periodEnd != currentPeriod);
+
+            item = new SubPeriodItem();
+
+            item.Period = currentPeriod;
+
+            if (periodDisplay == WebCst.CustomerSessions.Period.DisplayLevel.weekly)
+                item.Label = currentPeriod.Substring(4, 2);
+            else
+                item.Label = MonthString.GetCharacters(int.Parse(currentPeriod.Substring(4, 2)), cultureInfo, 1);
+
+            AppendPeriod(CustomerSession, periodType, currentPeriod, i, ref item, ref periodIndex, realPeriodBegin, realPeriodEnd, ref labBegin, ref labEnd, activePeriods);
+
+            if (item.Period == zoomDate)
+            {
+                item.IsSelected = true;
+                subPeriod.FirstPeriodLabel = item.PeriodLabel;
+            }
+            else
+                item.IsSelected = false;
+
+            subPeriod.Items.Add(item);
+
+            return subPeriod;
+        }
+
+        #region AppendPeriod
+        private void AppendPeriod(WebSession CustomerSession, WebCst.CustomerSessions.Period.Type periodType, string currentPeriod, int i, ref SubPeriodItem item, ref int periodIndex, string globalDateBegin, string globalDateEnd, ref string labBegin, ref string labEnd, List<string> activePeriods)
+        {
+            string tmpBegin = string.Empty;
+            string tmpEnd = string.Empty;
+
+            if (globalDateBegin.Length > 0)
+            {
+                tmpBegin = Dates.DateToString(
+                    Dates.Max(Dates.getZoomBeginningDate(currentPeriod, periodType),
+                    Dates.getPeriodBeginningDate(globalDateBegin, WebCst.CustomerSessions.Period.Type.dateToDate))
+                    , CustomerSession.SiteLanguage);
+            }
+            else
+            {
+                tmpBegin = Dates.DateToString(Dates.getZoomBeginningDate(currentPeriod, periodType), CustomerSession.SiteLanguage);
+            }
+
+            if (globalDateEnd.Length > 0)
+            {
+                tmpEnd = Dates.DateToString(
+                    Dates.Min(Dates.getZoomEndDate(currentPeriod, periodType),
+                    Dates.getPeriodEndDate(globalDateEnd, WebCst.CustomerSessions.Period.Type.dateToDate))
+                    , CustomerSession.SiteLanguage);
+            }
+            else
+            {
+                tmpEnd = Dates.DateToString(Dates.getZoomEndDate(currentPeriod, periodType), CustomerSession.SiteLanguage);
+            }
+            if (tmpEnd != tmpBegin)
+            {
+                item.PeriodLabel = string.Format("{0} {1} {2} {3}",
+                GestionWeb.GetWebWord(896, CustomerSession.SiteLanguage),
+                tmpBegin,
+                GestionWeb.GetWebWord(897, CustomerSession.SiteLanguage),
+                tmpEnd);
+            }
+            else
+            {
+                item.PeriodLabel = string.Format("{0}", tmpBegin);
+            }
+        }
+        #endregion
+    }
+}
