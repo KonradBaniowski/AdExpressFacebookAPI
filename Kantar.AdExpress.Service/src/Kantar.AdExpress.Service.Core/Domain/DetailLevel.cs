46242bc93e8c261e89fbf9a0414e3b77394f8247
yougilkt <youssef.yougil@kantarmedia.com>
2016-03-29 11:07:50 +0200

    Avancement Insertion (DetailLevel chargé, mais pas encoe pris en compte)

---
+++ Kantar.AdExpress.Service/src/Kantar.AdExpress.Service.Core/Domain/DetailLevel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kantar.AdExpress.Service.Core.Domain
+{
+    public class DetailLevel
+    {
+        public int level { get; set; }
+
+        public List<DetailLevelItems> Items { get; set; }
+    }
+}
---
+++ Kantar.AdExpress.Service/src/Kantar.AdExpress.Service.Core/Domain/DetailLevelItems.cs
@@ -0,0 +1,13 @@
+﻿namespace Kantar.AdExpress.Service.Core.Domain
+{
+    public class DetailLevelItems
+    {
+        public bool IsSelected { get; set; }
+
+        public string Label { get; set; }
+
+        public string DetailLabel { get; set; }
+
+        public string DetailLevel { get; set; }
+    }
+}
