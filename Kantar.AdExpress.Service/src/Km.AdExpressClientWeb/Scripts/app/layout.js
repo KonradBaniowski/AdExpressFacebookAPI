1c26e49e6b91a5b5237a4219fb7de4653545d067
savoury <wrap.romain@gmail.com>
2016-03-29 15:10:12 +0200

    Spinner global et message pour le dismiss

--- Kantar.AdExpress.Service/src/Km.AdExpressClientWeb/Scripts/app/layout.js
+++ Kantar.AdExpress.Service/src/Km.AdExpressClientWeb/Scripts/app/layout.js
@@ -4,10 +4,18 @@ $(document).ajaxStart(function () {
         keyboard: false,
         backdrop: 'static'
     });
-    console.log("Triggered ajaxStart handler.");
     $('#loader').modal('show');
+    $('#loader .modal-footer').delay(5000).fadeOut(200, function () {
+        $(this).show();
+    });
 });
 
 $(document).ajaxStop(function () {
+    $('#loader .modal-footer').dequeue();
+    $('#loader .modal-footer').hide();
     $('#loader').modal('hide');
+});
+
+$('#loader .modal-footer button').on('click', function () {
+    $(this).hide();
 });
