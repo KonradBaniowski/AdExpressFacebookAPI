65fd8d1579ce924ec066ffe21d3b8e59fe8c2805
yougilkt <youssef.yougil@kantarmedia.com>
2016-04-01 20:36:40 +0200

    Ajout Model InsertionCreative partegé

---
+++ Kantar.AdExpress.Service/src/Km.AdExpressClientWeb/Models/Shared/InsertionCreativeViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System;
+using Km.AdExpressClientWeb.Models.Shared;
+
+namespace Km.AdExpressClientWeb.Models.Shared
+{
+    public class InsertionCreativeViewModel
+    {
+        public List<string> paramsUrl { get; set; }
+        public int SiteLanguage { get; set; }
+
+    }
+
+}
