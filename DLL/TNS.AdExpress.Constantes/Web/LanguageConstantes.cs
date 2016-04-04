c50530893f769c395c1cf317a94ce0ab9306580b
SAAZ <s.allah.azzaza@gmail.com>
2016-04-04 12:44:41 +0200

    load all details of present/absent univers

--- DLL/TNS.AdExpress.Constantes/Web/LanguageConstantes.cs
+++ DLL/TNS.AdExpress.Constantes/Web/LanguageConstantes.cs
@@ -8,8 +8,9 @@ namespace TNS.AdExpress.Constantes.Web
   public  class LanguageConstantes
     {
         #region CODES OF MARKET'S LABELS
+        public const long Cancel = 000;
+        public const long Submit = 000;
         public const long MediaScheduleCode = 150;
-        public const long MediaScheduleDescriptionCode = 2200;
         public const long SaveUniversCode = 769;
         public const long LoadUniversCode = 770;
         public const long ResultsCode = 829;
@@ -17,41 +18,40 @@ namespace TNS.AdExpress.Constantes.Web
         public const long UserSavedUniversCode = 893;
         public const long SelectUniversGroup = 917;
         public const long ExceptionMsg = 922;
-        public const long ErrorMsgCode = 930;
         public const long NoSavedUniversCode = 930;
         public const long KeyWordLabelCode = 972;
         public const long Capacity = 1000;
+        public const long MediaSelected = 1487;
         public const long RefineCode = 2053;
+        public const long PresentAbsentCode = 2167;
+        public const long LostWonCode = 2169;
         public const long PortfolioCode = 2170;
+        public const long PresentAbsentDescriptionCode = 2196;
+        public const long LostWonDescriptionCode = 2198;
         public const long PortfolioDescriptionCode = 2199;
+        public const long MediaScheduleDescriptionCode = 2200;
         public const long SelectUnivers = 2261;
+        public const long ErrorOverLimit = 2264;//==> only 200 items
+        public const long LimitUniverses = 2265;
         public const long UniversLabel = 2268;
         public const long ExcludeCode = 2269;
         public const long IncludeCode = 2270;
         public const long BranchLabelCode = 2272;
         public const long ElementLabelCode = 2278;
+        public const long DeleteAllcode = 2279;
         public const long IncludedElements = 2281;
         public const long ExcludedElements = 2282;
         public const long SecurityMsg = 2285;
         public const long OverLimitMsgCode = 2286;
         public const long KeyWordDescriptionCode = 2287;
-        public const long TreeLabelcode = 2278;
-        public const long Cancel = 000;
-        public const long Submit = 000;
-        public const long LimitKeyword = 3033;
-        public const long LimitUniverses = 2265;
+        public const long Concurrent = 2869;
         public const long MininumInclude = 2299;
-        public const long MediaSelected = 1487;
-        public const long NoSupport = 3035;
+        public const long LimitKeyword = 3033;
         public const long ItemExceeded = 3034;
-        public const long LostWonCode = 2169;
-        public const long LostWonDescriptionCode = 2198;
-        public const long PresentAbsentCode = 2167;
-        public const long PresentAbsentDescriptionCode = 2196;
-        public const long DeleteAllcode = 2279;
+        public const long NoSupport = 3035;
         public const long ErrorOnlyOneItemAllowed = 3036;
-        public const long ErrorOverLimit = 2264;//==> only 200items
         public const long AddConcurrentCode = 3037;
+        
         #endregion
     }
 }
