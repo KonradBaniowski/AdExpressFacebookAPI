8950b71e5d84ce8af59c61ed0304e340f44233f8
dedson <dede.mussuma@kantarmedia.com>
2016-04-05 16:11:26 +0200

    Ressources + ajout controller insertion Export

---
+++ Kantar.AdExpress.Service/src/Km.AdExpressClientWeb/Controllers/InsertionsExportController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Km.AdExpressClientWeb.Controllers
+{
+    public class InsertionsExportController : Controller
+    {
+        // GET: InsertionsExport
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
