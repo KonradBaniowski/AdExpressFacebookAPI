da5bcc3647da121db8f8fc744e6665bbe0e67bad
SAAZ <s.allah.azzaza@gmail.com>
2016-04-13 14:35:43 +0200

    Move and rename univers

--- Kantar.AdExpress.Service/src/Km.AdExpressClientWeb/Scripts/Account/myAdExpress.js
+++ Kantar.AdExpress.Service/src/Km.AdExpressClientWeb/Scripts/Account/myAdExpress.js
@@ -1,26 +1,29 @@
 ﻿var idNewDirectory = "";
 var idOldDirectory = "";
 var universId = "";
+
+//opening the move session's modal
 $('.btnMoveResult').on('click', function () {
     $('#moveResult').modal('show');
     universId = $(this).attr("data-id");
     idOldDirectory = $(this).attr("data-directory-id");
 });
 
-
+//opening the rename session's modal
 $('.btnRenameResult').on('click', function () {
     $('#renameResult').modal('show');
     universId = $(this).attr("data-id");
 });
 
-$('#btnRenameUnivers').on('click', function () {
-    var name = $('#universName').val();
+//saving the session's new name
+$('#btnRenameResult').on('click', function () {
+    var name = $('#newResultName').val();
     var params = {
         name: name,
         universId: universId
     };
     $.ajax({
-        url: '/Universe/RenameUnivers',
+        url: '/Universe/RenameSession',
         contentType: 'application/json',
         type: 'POST',
         datatype: 'JSON',
@@ -32,25 +35,28 @@ $('#btnRenameUnivers').on('click', function () {
         success: function (response) {
             $('#renameResult').modal('hide');
             //Reload the page
-            $.ajax({
-                url: '/Home/ReloadSession',
-                type: 'POST',
-                data: params,
-                error: function (data) {
-                    bootbox.alert(data);
-                },
-                success: function (data) {
-                    $("#Result").html(data);
-                }
-            });
-                    bootbox.alert("Success");
+            if (response == "Success") {
+                //Reload the page
+                $.ajax({
+                    url: '/Home/ReloadSession',
+                    type: 'POST',
+                    data: params,
+                    error: function (data) {
+                        bootbox.alert(data);
+                    },
+                    success: function (data) {
+                        $("#Result").html(data);
+                    }
+                });
+            }
+            bootbox.alert(response);
         }
     });    
 });
 
-
-$('#btnMoveUnivers').on('click', function () {
-    var idNewDirectory = $('.input-group-btn').find('.btn.btn-default.form-control.bg-black-only').attr("data-result");
+//saving the session under the new diretory
+$('#btnMoveResult').on('click', function () {
+    var idNewDirectory = $('.input-group-btn').find('.btn.btn-default.form-control.bg-black-only.idResult').attr("data-result");
     var params = {
         idOldDirectory: idOldDirectory,
         idNewDirectory: idNewDirectory,
@@ -85,6 +91,8 @@ $('#btnMoveUnivers').on('click', function () {
     });
 });
 
+
+// Managing sessions' and unverses' ddl
 $(".dropdown-menu.bg-blue.form-control li > a").on('click', function (e) {
     e.preventDefault();
     var selText = $(this).text();
@@ -96,4 +104,94 @@ $(".dropdown-menu.bg-blue.form-control li > a").on('click', function (e) {
     $(this).parents('.input-group-btn').find('.btn.btn-default.form-control.bg-black-only').html(selText + '<span class="caret"></span>');
     $(this).parents('.input-group-btn').find('.btn.btn-default.form-control.bg-black-only').attr("data-result", selValue);
 
+});
+
+
+//opening the move univers' modal
+$('.btnMoveUnivers').on('click', function () {
+    $('#moveUnivers').modal('show');
+    universId = $(this).attr("data-id");
+    idOldDirectory = $(this).attr("data-directory-id");
+});
+
+//opening the save univers' modal
+$('.btnRenameUnivers').on('click', function () {
+    $('#renameUnivers').modal('show');
+    universId = $(this).attr("data-id");
+});
+
+//saving the univers' new name
+$('#btnRenameUnivers').on('click', function () {
+    var name = $('#newUniversName').val();
+    var params = {
+        name: name,
+        universId: universId
+    };
+    $.ajax({
+        url: '/Universe/RenameUnivers',
+        contentType: 'application/json',
+        type: 'POST',
+        datatype: 'JSON',
+        data: JSON.stringify(params),
+        error: function (xmlHttpRequest, errorText, thrownError) {
+            $('#renameResult').modal('hide');
+            bootbox.alert("An error occurred while processing your request.");
+        },
+        success: function (response) {
+            $('#renameUnivers').modal('hide');
+            if (response == "Success") {
+                //Reload the page
+                $.ajax({
+                    url: '/Home/ReloadUnivers',
+                    type: 'POST',
+                    data: params,
+                    error: function (data) {
+                        bootbox.alert(data);
+                    },
+                    success: function (data) {
+                        $("#Univers").html(data);
+                    }
+                });                
+            }
+                bootbox.alert(response);
+
+        }
+    });
+});
+
+//saving the univers under the new diretory
+$('#btnMoveUnivers').on('click', function () {
+    var idNewDirectory = $('.input-group-btn').find('.btn.btn-default.form-control.bg-black-only.idUnivers').attr("data-result");
+    var params = {
+        idOldDirectory: idOldDirectory,
+        idNewDirectory: idNewDirectory,
+        id: universId
+    };
+    $.ajax({
+        url: '/Universe/MoveUnivers',
+        contentType: 'application/json',
+        type: 'POST',
+        datatype: 'JSON',
+        data: JSON.stringify(params),
+        error: function (xmlHttpRequest, errorText, thrownError) {
+            $('#moveUnivers').modal('hide');
+            bootbox.alert("An error occurred while processing your request.");
+        },
+        success: function (response) {
+            $('#moveUnivers').modal('hide');
+            //Reload the page
+            $.ajax({
+                url: '/Home/ReloadUnivers',
+                type: 'POST',
+                data: params,
+                error: function (data) {
+                    bootbox.alert(data);
+                },
+                success: function (data) {
+                    $("#Univers").html(data);
+                }
+            });
+            bootbox.alert("Success");
+        }
+    });
 });
