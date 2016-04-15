576e55e3e2b0683763d4629d9cfc8bd44c747154
savoury <wrap.romain@gmail.com>
2016-04-15 11:40:40 +0200

    Animation fun

--- Kantar.AdExpress.Service/src/Km.AdExpressClientWeb/Scripts/app/layout.js
+++ Kantar.AdExpress.Service/src/Km.AdExpressClientWeb/Scripts/app/layout.js
@@ -8,7 +8,10 @@ $(document).ajaxStart(function () {
     $('#loader .modal-footer').delay(5000).fadeOut(200, function () {
         $(this).show();
     });
+
+
 });
+
 
 $(document).ajaxStop(function () {
     //$('#loader .modal-footer').dequeue();
@@ -16,6 +19,27 @@ $(document).ajaxStop(function () {
     $('#loader').modal('hide');
 });
 
+$(document).ready(function () {
+    var options = {
+        useEasing: true,
+        useGrouping: true,
+        separator: ',',
+        decimal: '.',
+        prefix: '',
+        suffix: ''
+    };
+
+    $.each($('.animationFun'), function (index, value) {
+        var number = $(value).attr('data-value');
+        var demo = new CountUp("#fun"+index, 0, number, 0, 3, options);
+        demo.start();
+    });
+    //var demo = new CountUp("#animationFun", 0, 2854, 0, 5, options);
+   
+
+});
+
 $('#loader .modal-footer button').on('click', function () {
     $(this).hide();
 });
+
