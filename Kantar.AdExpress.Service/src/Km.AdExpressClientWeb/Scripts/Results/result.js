ba7f719ba406409f8f55810bdf8d9d07a5f9ecc7
SAAZ <s.allah.azzaza@gmail.com>
2016-04-29 17:11:51 +0200

    #78

--- Kantar.AdExpress.Service/src/Km.AdExpressClientWeb/Scripts/Results/result.js
+++ Kantar.AdExpress.Service/src/Km.AdExpressClientWeb/Scripts/Results/result.js
@@ -1,4 +1,32 @@
 ﻿$(document).on('click', '#btn-create-alert', function (event) {
     $('#btn-create-alert').off();
-    bootbox.alert("Feature under construction.");    
+    var params = {
+
+    };
+    $.ajax({
+        url: '/Alert/CreateAlert',
+        contentType: 'application/json',
+        type: 'POST',
+        datatype: 'JSON',
+        //data: JSON.stringify(params),
+        error: function (xmlHttpRequest, errorText, thrownError) {
+            bootbox.alert("An error occurred while processing your request.");
+        },
+        success: function (response) {
+            $('#alertModal .modal-content').html('');
+            $('#alertModal .modal-content').append(response);
+            $('#alertModal').modal('show');
+        }
+    });  
+});
+
+$('#datetimepicker').datetimepicker({
+    inline: true,
+    locale: 'fr',
+    format: 'DD/MM/YYYY',
+    useCurrent: false,
+    //minDate: minDate,
+    //maxDate: maxDate
+}).on('dp.change', function (e) {
+    $('#end-date-value').val($('#datetimepicker-end').data('date'));
 });
