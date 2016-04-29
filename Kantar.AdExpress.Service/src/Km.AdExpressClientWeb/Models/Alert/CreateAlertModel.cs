52804eef66f510c41cbf56d997626169f09b898d
SAAZ <s.allah.azzaza@gmail.com>
2016-04-29 10:39:32 +0200

    #78 Add CreateAlert's model

---
+++ Kantar.AdExpress.Service/src/Km.AdExpressClientWeb/Models/Alert/CreateAlertModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Km.AdExpressClientWeb.Models.Alert
+{
+    public class CreateAlertModel
+    {
+    }
+}
