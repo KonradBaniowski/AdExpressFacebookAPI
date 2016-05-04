516cc6559cdf3ba91ca5ec0ec93ae73ad3de4f8b
dedson <dede.mussuma@kantarmedia.com>
2016-05-04 17:19:24 +0200

    Changement de langues

---
+++ Kantar.AdExpress.Service/src/Km.AdExpressClientWeb/Scripts/app/language-choice.js
@@ -0,0 +1,30 @@
+﻿if (typeof jQuery === "undefined") { throw new Error("Language selector requires jQuery") }
+
+$('.navbar-nav > li > .dropdown-menu > li  a').on('click', function (event, returnUrl) {
+    event.preventDefault();
+    var languageId = $(this).attr("data-language-id");
+    var th = $(this).addClass("active");
+    var selText = $(this).text();
+    var par = $(this).parents().find('#language-choice').html(selText + '<span class="caret"></span>');
+
+    var params = {
+        returnUrl: $('#current-url').val(),
+        siteLanguage: languageId
+    };
+
+    $.ajax({
+        url: '/Home/ChangeLanguage',
+        contentType: 'application/json',
+        type: 'POST',
+        datatype: 'JSON',
+        data: JSON.stringify(params),
+        error: function (xmlHttpRequest, errorText, thrownError) {
+
+        },
+        success: function (data) {
+            if (data != null) {
+                document.location = data.RedirectUrl;
+            }
+        }
+    });
+});
--- Kantar.AdExpress.Service/src/Km.AdExpressClientWeb/Scripts/module-selection/module-selection.js
+++ Kantar.AdExpress.Service/src/Km.AdExpressClientWeb/Scripts/module-selection/module-selection.js
@@ -28,34 +28,7 @@ $(function () {
         });
     }
 
-    $('.navbar-nav > li > .dropdown-menu > li  a').on('click', function (event, returnUrl) {
-        event.preventDefault();
-        var languageId = $(this).attr("data-language-id");
-        var th = $(this).addClass("active");
-        var selText = $(this).text();
-        var par = $(this).parents().find('#language-choice').html(selText + '<span class="caret"></span>');
-       
-        var params = {
-            returnUrl: $('#current-url').val(),
-            siteLanguage: languageId
-        };
-
-        $.ajax({
-            url: '/Home/ChangeLanguage',
-            contentType: 'application/json',
-            type: 'POST',
-            datatype: 'JSON',
-            data: JSON.stringify(params),
-            error: function (xmlHttpRequest, errorText, thrownError) {
-
-            },
-            success: function (data) {
-                if (data != null) {
-                    document.location = data.RedirectUrl;
-                }
-            }
-        });
-    });
+   
 
 
     
