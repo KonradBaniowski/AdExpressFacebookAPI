3485ca6e3c1bd08e2984b6eb991e13a8011948f1
SAAZ <s.allah.azzaza@gmail.com>
2016-05-11 14:25:15 +0200

    #108 Part 1

---
+++ Kantar.AdExpress.Service/src/Km.AdExpressClientWeb/Models/Shared/ExportResultModel.cs
@@ -0,0 +1,21 @@
+﻿using KM.Framework.Constantes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Km.AdExpressClientWeb.Models.Shared
+{
+    public class ExportResultModel
+    {
+        [Required]
+        public string FileName { get; set; }
+        [Required]
+        public string Email { get; set; }
+        public List<SelectListItem> ExportType { get; set; }
+        public int SelectedExportTypeId { get; set; }
+        public Labels Labels { get; set; }
+    }
+}
