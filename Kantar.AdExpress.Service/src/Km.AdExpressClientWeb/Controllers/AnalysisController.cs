eec150385fdfe8968fcb670724569d9895ca1d44
SAAZ <s.allah.azzaza@gmail.com>
2016-05-17 10:18:52 +0200

    Ajout du controllor pour l analyse secto

---
+++ Kantar.AdExpress.Service/src/Km.AdExpressClientWeb/Controllers/AnalysisController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Km.AdExpressClientWeb.Controllers
+{
+    [Authorize]
+    public class AnalysisController : Controller
+    {
+        // GET: Analysis
+        public ActionResult Market()
+        {
+            return View();
+        }
+    }
+}
