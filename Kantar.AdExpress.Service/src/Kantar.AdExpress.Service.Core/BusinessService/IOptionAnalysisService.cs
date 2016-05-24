3f4565f56ed89ffd03537007d50303549656c50e
yrkaina <youssef.rkaina@gmail.com>
2016-05-24 11:49:44 +0200

    Add Option for PRODUCT CLASS ANALYSIS: DETAILED REPORTS

---
+++ Kantar.AdExpress.Service/src/Kantar.AdExpress.Service.Core/BusinessService/IOptionAnalysisService.cs
@@ -0,0 +1,15 @@
+﻿using Kantar.AdExpress.Service.Core.Domain.ResultOptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kantar.AdExpress.Service.Core.BusinessService
+{
+    public interface IOptionAnalysisService
+    {
+        OptionsAnalysis GetOptions(string idWebSession);
+        void SetOptions(string idWebSession, UserAnalysisFilter userFilter);
+    }
+}
