c6f04e2d3e9fbb311ba1088e4e3854965cceb792
yougilkt <youssef.yougil@kantarmedia.com>
2016-06-24 15:57:59 +0200

    Export Excel Module facebook : PART II

---
+++ DLL/TNS.AdExpress.Domain/Results/GridResultExport.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace TNS.AdExpress.Domain.Results
+{
+    public class GridResultExport
+    {
+        public List<InfragisticData> Data { get; set; }
+        public List<InfragisticColumn> Columns { get; set; }
+        public bool HasData { get; set; }
+    }
+}
