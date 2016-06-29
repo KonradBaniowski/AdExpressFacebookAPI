6db8ca2dba3a8d40ba8b4d406806aac5aac5856e
yougilkt <youssef.yougil@kantarmedia.com>
2016-06-29 18:57:33 +0200

    PostModel

---
+++ Kantar.AdExpress.Service/src/Kantar.AdExpress.Service.Core/Domain/PostModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Kantar.AdExpress.Service.Core.Domain
+{
+    public class PostModel
+    {
+        public int idLogin { get; set; }
+        public long beginDate { get; set; }
+        public long endDate { get; set; }
+        public List<long> idAdvertisers { get; set; }
+        public List<long> idBrands { get; set; }
+    }
+}
