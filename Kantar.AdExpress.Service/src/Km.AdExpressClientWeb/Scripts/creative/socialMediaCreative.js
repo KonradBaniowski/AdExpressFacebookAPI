fa4776193a8cf9986d4fe664a081522a3d345895
yougilkt <youssef.yougil@kantarmedia.com>
2016-07-01 14:52:13 +0200

    Js pour socialmediacreative

--- Kantar.AdExpress.Service/src/Km.AdExpressClientWeb/Scripts/creative/socialMediaCreative.js
+++ Kantar.AdExpress.Service/src/Km.AdExpressClientWeb/Scripts/creative/socialMediaCreative.js
@@ -15,20 +15,6 @@ var ds;
 var cols;
 var colsFixed;
 var needFixedColumns = false;
-var periodType = "Mois";
-
-function GenericDetailLevelFilter() {
-    this.L1DetailValue = -1;
-    this.L2DetailValue = -1;
-    this.L3DetailValue = -1;
-}
-
-function UserFilter() {
-    this.GenericDetailLevelFilter = new GenericDetailLevelFilter();
-    this.isVehicleChanged = true;
-}
-
-var userFilter = new UserFilter();
 
 var renderGrid = function (success, error) {
 
@@ -140,26 +126,19 @@ function CallInsertionsResult() {
     });
 }
 
-//** Important charge les images au fur et a mesure que le teableau s'affiche
+CallInsertionsResult();
+
+
+//** charge les images au fur et a mesure que le teableau s'affiche (image post facebook)
 $("#grid").on("igtreegridrowsrendered igtreegridrowexpanding igtreegridrowcollapsing", function (evt, ui) {
-    AutoPlayVisu();
 
-    $(".carousel").each(function (index) {
-        $("#visuCarou" + index.toString()).carousel("pause");
-        $("#visuCarou" + index.toString()).find('.left').click(function () {
-            $("#visuCarou" + index.toString()).carousel("prev");
-        });
-        $("#visuCarou" + index.toString()).find('.right').click(function () {
-            $("#visuCarou" + index.toString()).carousel("next");
-        });
-    });
-
-    $(".triggerModal > .fa").each(function () {
-        $(this).addClass('iconInsertionCreative iconInsertionCreative' + userFilter.paramsUrl.idVehicle + '');
+    $(".imgPostsFacebook").each(function () {
+        var datas = $(this).attr('data-post').toString();
+        var link = "PostsFacebook/" + datas.substring(0, 1) + "/" + datas.substring(1, 4) + "/" + datas + "_Post.png"
+        $(this).attr("src", link);
     });
 
 });
-CallInsertionsResult();
 
 //Resize de la page, la longuer du tableau egalament
 $(window).resize(function () {
