e44f8b342c3c55c27bb2fe722b698eae303ff51f
savoury <wrap.romain@gmail.com>
2016-07-12 15:25:44 +0200

    Mapping Data Search

---
+++ src/Facebook.Service.Core/DomainModels/AdExprSchema/DataSearch.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Facebook.Service.Core.DomainModels.AdExprSchema
+{
+    public class DataSearch: Data
+    {
+        public long DateMediaNum { get; set; }
+        public long IdDataSearch { get; set; }
+        public long IdLanguageData { get; set; }
+        public long IdProduct { get; set; }
+        public long ExpenditureEuro { get; set; }
+    }
+}
