96ac86be44307900615c3ca427d9e405ccd3c9f0
dedson <dede.mussuma@kantarmedia.com>
2016-07-20 14:51:15 +0200

    recap tables

---
+++ src/Facebook.Service.Core/DomainModels/RecpaSchema/Brand.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Facebook.Service.Core.DomainModels.RecpaSchema
+{
+   public class Brand
+    {
+
+        public long Id { get; set; }
+        public long IdLanguage { get; set; }
+        public string BrandLabel { get; set; }
+        public long Activation { get; set; }
+    }
+}
