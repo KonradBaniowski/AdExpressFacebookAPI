f5e7aa74efab9f23ab38148e375a3ca2a1bb380b
KT\dede.mussuma <dede.mussuma@kantarmedia.com>
2016-11-23 14:26:48 +0100

    Add result controller and Views Files

---
+++ Kantar.AdExpress.Service/src/Km.AdExpressClientWeb/Controllers/ProgramAnalysisController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Km.AdExpressClientWeb.Controllers
+{
+    public class ProgramAnalysisController : Controller
+    {
+        // GET: ProgramAnalysis
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
