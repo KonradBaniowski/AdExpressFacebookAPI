0db43a188bbbebf89baebe7a5e00002d6c56373c
yrkaina <youssef.rkaina@gmail.com>
2018-06-27 16:23:46 +0200

    #454 - Add Media Schedule Rules Layer for Turkey

---
+++ DLL/TNS.AdExpressI.MediaSchedule.Turkey/MediaScheduleResults.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TNS.AdExpress.Web.Core.Selection;
+using TNS.AdExpress.Web.Core.Sessions;
+
+namespace TNS.AdExpressI.MediaSchedule.Turkey
+{
+    public class MediaScheduleResults : MediaSchedule.MediaScheduleResults
+    {
+        #region Constructors
+        /// <summary>
+        /// Default Constructor
+        /// </summary>
+        /// <param name="session">User Session</param>
+        /// <param name="period">Report Period</param>
+        public MediaScheduleResults(WebSession session, MediaSchedulePeriod period) : base(session, period) { }
+        /// <summary>
+        /// Constructor of a Media Schedule on a specifi vehicle
+        /// </summary>
+        /// <param name="session">User Session</param>
+        /// <param name="period">Report Period</param>
+        /// <param name="idVehicle">Vehicle Filter</param>
+        public MediaScheduleResults(WebSession session, MediaSchedulePeriod period, Int64 idVehicle) : base(session, period, idVehicle) { }
+        /// <summary>
+        /// Constructor of a Media Schedule on a zoomed period
+        /// </summary>
+        /// <param name="session">User Session</param>
+        /// <param name="period">Report Period</param>
+        /// <param name="zoom">Report zoom</param>
+        public MediaScheduleResults(WebSession session, MediaSchedulePeriod period, string zoom) : base(session, period, zoom) { }
+        /// <summary>
+        /// Constructor of a Media Schedule on a specifi vehicle and a zoomed period
+        /// </summary>
+        /// <param name="session">User Session</param>
+        /// <param name="period">Report Period</param>
+        /// <param name="idVehicle">Vehicle Id</param>
+        /// <param name="zoom">Report zoom</param>
+        public MediaScheduleResults(WebSession session, MediaSchedulePeriod period, Int64 idVehicle, string zoom) : base(session, period, idVehicle, zoom) { }
+        #endregion
+    }
+}
