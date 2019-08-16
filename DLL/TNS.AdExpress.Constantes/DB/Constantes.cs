9483415b3630541b90a37187aad5ec7dc6bed93b
KT\dede.mussuma <dede.mussuma@kantarmedia.com>
2019-08-16 16:13:50 +0200

    flag multimedia extended, audio digitan , paid search

--- DLL/TNS.AdExpress.Constantes/DB/Constantes.cs
+++ DLL/TNS.AdExpress.Constantes/DB/Constantes.cs
@@ -1875,6 +1875,18 @@ namespace TNS.AdExpress.Constantes.DB{
         /// Identifiant du flag accès aux codes SIRET
         /// </summary>
         public const Int64 ID_CODE_SIRET_ACCESS_FLAG = 368;
+        /// <summary>
+        /// Identifiant du flag accès au média PLURIMEDIA ELARGI
+        /// </summary>
+        public const Int64 ID_MULTIMEDIA_EXTENDED_ACCESS_FLAG = 378;
+        /// <summary>
+        /// Identifiant du flag accès au média PAID SEARCH
+        /// </summary>
+        public const Int64 ID_PAID_SEARCH_ACCESS_FLAG = 379;
+        /// <summary>
+        /// Identifiant du flag accès au média AUDIO DIGITAL
+        /// </summary>
+        public const Int64 ID_AUDIO_DIGITAL_ACCESS_FLAG = 380;
 
     }
 	#endregion
